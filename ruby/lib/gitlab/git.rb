c10ce9ae95cc53840d75449245d56287c9851558
Jacob Vosmaer <jacob@gitlab.com>
2017-08-10 15:22:54 +0200

    Add comments to the magic load file

--- ruby/lib/gitlab/git.rb
+++ ruby/lib/gitlab/git.rb
@@ -1,17 +1,24 @@
+# External dependencies of Gitlab::Git
 require 'rugged'
 require 'linguist'
 
+# Ruby on Rails mix-ins that GitLab::Git code relies on
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/numeric/bytes'
 require 'active_support/core_ext/module/delegation'
 require 'active_support/core_ext/enumerable'
 
+# We split our mock implementation of Gitlab::GitalyClient into a separate file
+require_relative 'gitaly_client.rb'
+
 vendor_gitlab_git = '../../vendor/gitlab_git/'
+
+# Some later requires are order-sensitive. Manually require whatever we need.
 require_relative File.join(vendor_gitlab_git, 'lib/gitlab/encoding_helper.rb')
 require_relative File.join(vendor_gitlab_git, 'lib/gitlab/git.rb')
 require_relative File.join(vendor_gitlab_git, 'lib/gitlab/git/ref.rb')
-require_relative 'gitaly_client.rb'
 
+# Require all .rb files we can find in the vendored gitlab/git directory
 dir = File.expand_path(File.join('..', vendor_gitlab_git, 'lib/gitlab/'), __FILE__)
 Dir["#{dir}/git/**/*.rb"].each do |ruby_file|
   require_relative ruby_file.sub(dir, File.join(vendor_gitlab_git, 'lib/gitlab/')).sub(%r{^/*}, '')
@@ -48,7 +55,8 @@ module Gitlab
         @rugged = Rugged::Repository.new(path)
         @attributes = Gitlab::Git::Attributes.new(path)
       end
-  
+
+      # Bypass the CircuitBreaker class which needs Redis
       def rugged
         @rugged
       end
