fbe496377d93697367f67915828825a3be6ff590
Jacob Vosmaer <jacob@gitlab.com>
2017-08-10 18:24:53 +0200

    Add some comments to gitaly_client.rb

--- ruby/lib/gitlab/gitaly_client.rb
+++ ruby/lib/gitlab/gitaly_client.rb
@@ -1,8 +1,10 @@
 module Gitlab
   module GitalyClient
     class << self
+      # In case we hit a method that tries to do a Gitaly RPC, prevent this.
+      # We also don't want to instrument the block.
       def migrate(*args)
-        yield false
+        yield false # 'false' means 'don't use gitaly for this block'
       end
     end
   end
