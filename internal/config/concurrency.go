921c178db4ab1cbbf569755d843da741102a80e9
Andrew Newdigate <andrew@gitlab.com>
2017-09-27 13:04:42 +0100

    Fixed comment [skip ci]

--- internal/config/concurrency.go
+++ internal/config/concurrency.go
@@ -4,7 +4,7 @@ import (
 	"gitlab.com/gitlab-org/gitaly/internal/middleware/limithandler"
 )
 
-// ConfigureConcurrencyLimits configures the sentry DSN
+// ConfigureConcurrencyLimits configures the per-repo, per RPC rate limits
 func ConfigureConcurrencyLimits() {
 	maxConcurrencyPerRepoPerRPC := make(map[string]int64)
 
