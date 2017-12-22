43df2f14beafeb55e3d6f570f45b03ebd23b457f
Jacob Vosmaer <jacob@gitlab.com>
2017-12-22 12:57:54 +0100

    Add rugged check

--- ruby/spec/gitaly/ref_service_spec.rb
+++ ruby/spec/gitaly/ref_service_spec.rb
@@ -9,15 +9,19 @@ describe Gitaly::RefService do
 
   describe 'CreateBranch' do
     it 'can create a branch' do
+      repo = test_repo_mutable
       branch_name = 'branch-' + SecureRandom.hex(10)
       request = Gitaly::CreateBranchRequest.new(
-        repository: test_repo_mutable,
+        repository: repo,
         name: branch_name,
         start_point: 'master'
       )
       response = service_stub.create_branch(request)
 
       expect(response.status).to eq(:OK)
+
+      rugged = rugged_from_gitaly(repo)
+      expect(response.branch.target_commit.id).to eq(rugged.branches[branch_name].target_id)
     end
   end
 end
--- ruby/spec/test_repo_helper.rb
+++ ruby/spec/test_repo_helper.rb
@@ -1,6 +1,7 @@
 require 'fileutils'
 require 'securerandom'
 require 'gitaly'
+require 'rugged'
 
 DEFAULT_STORAGE_DIR = File.expand_path('../../tmp/repositories', __FILE__)
 DEFAULT_STORAGE_NAME = 'default'.freeze
@@ -24,6 +25,16 @@ module TestRepo
     relative_path = "mutable-#{SecureRandom.hex(6)}.git"
     TestRepo.clone_new_repo!(File.join(DEFAULT_STORAGE_DIR, relative_path))
     Gitaly::Repository.new(storage_name: DEFAULT_STORAGE_NAME, relative_path: relative_path)
+  end
+
+  def rugged_from_gitaly(gitaly_repo)
+    Rugged::Repository.new(repo_path_from_gitaly(gitaly_repo))
+  end
+
+  def repo_path_from_gitaly(gitaly_repo)
+    storage_name = gitaly_repo.storage_name
+    raise "this helper does not know storage #{storage_name.inspect}" unless storage_name == DEFAULT_STORAGE_NAME
+    File.join(DEFAULT_STORAGE_DIR, gitaly_repo.relative_path)
   end
 
   def self.clone_new_repo!(destination)
