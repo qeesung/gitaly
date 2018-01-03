cd416c5d4bcdba95c0b33e08e2abda6075266ee8
Jacob Vosmaer <jacob@gitlab.com>
2018-01-03 12:36:00 +0100

    Use 'subject'

--- ruby/spec/gitaly/repository_service_spec.rb
+++ ruby/spec/gitaly/repository_service_spec.rb
@@ -4,18 +4,18 @@ describe Gitaly::RepositoryService do
   include IntegrationClient
   include TestRepo
 
-  let(:service_stub) { gitaly_stub(:RepositoryService) }
+  subject { gitaly_stub(:RepositoryService) }
 
   describe 'RepositoryExists' do
     it 'returns false if the repository does not exist' do
       request = Gitaly::RepositoryExistsRequest.new(repository: gitaly_repo('default', 'foobar.git'))
-      response = service_stub.repository_exists(request)
+      response = subject.repository_exists(request)
       expect(response.exists).to eq(false)
     end
 
     it 'returns true if the repository exists' do
       request = Gitaly::RepositoryExistsRequest.new(repository: test_repo_read_only)
-      response = service_stub.repository_exists(request)
+      response = subject.repository_exists(request)
       expect(response.exists).to eq(true)
     end
   end
