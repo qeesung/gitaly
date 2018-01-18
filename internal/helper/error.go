a1e240789a87e4d5dbfb2dd62a25d3263c8c67d3
Jacob Vosmaer <jacob@gitlab.com>
2018-01-18 16:54:00 +0100

    Remove grpc.Errorf

--- internal/helper/error.go
+++ internal/helper/error.go
@@ -3,16 +3,17 @@ package helper
 import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // Unimplemented is a Go error with gRPC error code 'Unimplemented'
-var Unimplemented = grpc.Errorf(codes.Unimplemented, "this rpc is not implemented")
+var Unimplemented = status.Errorf(codes.Unimplemented, "this rpc is not implemented")
 
 // DecorateError unless it's already a grpc error.
 //  If given nil it will return nil.
 func DecorateError(code codes.Code, err error) error {
 	if err != nil && grpc.Code(err) == codes.Unknown {
-		return grpc.Errorf(code, "%v", err)
+		return status.Errorf(code, "%v", err)
 	}
 	return err
 }
