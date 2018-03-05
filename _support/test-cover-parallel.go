0b58837473d9a83daea369897b673dc81f1864e9
Jacob Vosmaer <jacob@gitlab.com>
2018-03-05 15:50:59 +0100

    bound concurrency by gomaxprocs

--- _support/test-cover-parallel.go
+++ _support/test-cover-parallel.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -12,10 +13,6 @@ import (
 
 const (
 	progName = "test-cover-parallel.go"
-)
-
-var (
-	numWorkers = 2 //runtime.GOMAXPROCS(0)
 )
 
 func main() {
@@ -26,6 +23,12 @@ func main() {
 	outDir := os.Args[1]
 	packages := os.Args[2:]
 	baseArgs := []string{"go", "test", fmt.Sprintf("-coverpkg=%s", strings.Join(packages, ","))}
+
+	numWorkers = 2
+	if maxprocs := runtime.GOMAXPROCS(0); maxprocs < numWorkers {
+		numWorkers = maxprocs
+	}
+	log.Printf("running 'go test -cover' in parallel with %d processes", numWorkers)
 
 	packageChan := make(chan string)
 	wg := &sync.WaitGroup{}
@@ -54,11 +57,11 @@ func handlePackage(p string, outDir string, baseArgs []string) {
 		fmt.Sprintf("-coverprofile=%s/unit-%s.out", outDir, strings.Replace(p, "/", "_", -1)),
 		p,
 	)
+
 	start := time.Now()
 	cmd := exec.Command(args[0], args[1:]...)
-	//	cmd.Stdout = os.Stdout
-	//	cmd.Stderr = os.Stderr // contains too much rubbish
 	err := cmd.Run()
+
 	duration := time.Since(start)
 	status := fmt.Sprintf("%s\t%.3fs", p, duration.Seconds())
 	if err != nil {
