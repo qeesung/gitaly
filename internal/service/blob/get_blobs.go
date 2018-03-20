8c11281a16d52aeaf0282b4d352c8836aabf97fa
Jacob Vosmaer <jacob@gitlab.com>
2018-03-20 17:11:10 +0100

    catfile.C in GetBlobs

--- internal/service/blob/get_blobs.go
+++ internal/service/blob/get_blobs.go
@@ -1,8 +1,6 @@
 package blob
 
 import (
-	"bufio"
-	"fmt"
 	"io"
 	"io/ioutil"
 
@@ -16,101 +14,102 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func getBlobsHandler(req *pb.GetBlobsRequest, stream pb.BlobService_GetBlobsServer, c *catfile.C) catfile.Handler {
-	return func(stdin io.Writer, stdout *bufio.Reader) error {
-		for _, revisionPath := range req.RevisionPaths {
-			revision := revisionPath.Revision
-			path := revisionPath.Path
+func sendGetBlobsResponse(req *pb.GetBlobsRequest, stream pb.BlobService_GetBlobsServer, c *catfile.C) error {
+	for _, revisionPath := range req.RevisionPaths {
+		revision := revisionPath.Revision
+		path := revisionPath.Path
 
-			treeEntry, err := commit.TreeEntryForRevisionAndPath(c, revision, string(path))
+		treeEntry, err := commit.TreeEntryForRevisionAndPath(c, revision, string(path))
+		if err != nil {
+			return err
+		}
+
+		response := &pb.GetBlobsResponse{Revision: revision, Path: path}
+
+		if treeEntry == nil || len(treeEntry.Oid) == 0 {
+			if err := stream.Send(response); err != nil {
+				return status.Errorf(codes.Unavailable, "GetBlobs: send: %v", err)
+			}
+
+			continue
+		}
+
+		response.Mode = treeEntry.Mode
+		response.Oid = treeEntry.Oid
+
+		if treeEntry.Type == pb.TreeEntry_COMMIT {
+			response.IsSubmodule = true
+
+			if err := stream.Send(response); err != nil {
+				return status.Errorf(codes.Unavailable, "GetBlobs: send: %v", err)
+			}
+
+			continue
+		}
+
+		objectInfo, err := c.Info(treeEntry.Oid)
+		if err != nil {
+			return status.Errorf(codes.Internal, "GetBlobs: %v", err)
+		}
+		if objectInfo.Type != "blob" {
+			return status.Errorf(codes.InvalidArgument, "GetBlobs: object at %s:%s is %s, not blob", revision, path, objectInfo.Type)
+		}
+
+		blobReader, err := c.Blob(objectInfo.Oid)
+		if err != nil {
+			return err
+		}
+
+		response.Size = objectInfo.Size
+
+		var readLimit int64
+		if req.Limit < 0 || req.Limit > objectInfo.Size {
+			readLimit = objectInfo.Size
+		} else {
+			readLimit = req.Limit
+		}
+
+		if readLimit == 0 {
+			if err := stream.Send(response); err != nil {
+				return status.Errorf(codes.Unavailable, "GetBlobs: send: %v", err)
+			}
+		} else {
+			sw := streamio.NewWriter(func(p []byte) error {
+				msg := &pb.GetBlobsResponse{}
+				if response != nil {
+					msg = response
+					response = nil
+				}
+
+				msg.Data = p
+
+				return stream.Send(msg)
+			})
+
+			sent, err := io.Copy(sw, io.LimitReader(blobReader, readLimit))
 			if err != nil {
-				return err
+				return status.Errorf(codes.Unavailable, "GetBlobs: send: %v", err)
 			}
-
-			response := &pb.GetBlobsResponse{Revision: revision, Path: path}
-
-			if treeEntry == nil || len(treeEntry.Oid) == 0 {
-				if err := stream.Send(response); err != nil {
-					return status.Errorf(codes.Unavailable, "GetBlobs: send: %v", err)
-				}
-
-				continue
-			}
-
-			response.Mode = treeEntry.Mode
-			response.Oid = treeEntry.Oid
-
-			if treeEntry.Type == pb.TreeEntry_COMMIT {
-				response.IsSubmodule = true
-
-				if err := stream.Send(response); err != nil {
-					return status.Errorf(codes.Unavailable, "GetBlobs: send: %v", err)
-				}
-
-				continue
-			}
-
-			fmt.Fprintln(stdin, treeEntry.Oid)
-
-			objectInfo, err := catfile.ParseObjectInfo(stdout)
-			if err != nil {
-				return status.Errorf(codes.Internal, "GetBlobs: %v", err)
-			}
-			if objectInfo.Type != "blob" {
-				return status.Errorf(codes.InvalidArgument, "GetBlobs: object at %s:%s is %s, not blob", revision, path, objectInfo.Type)
-			}
-
-			response.Size = objectInfo.Size
-
-			var readLimit int64
-			if req.Limit < 0 || req.Limit > objectInfo.Size {
-				readLimit = objectInfo.Size
-			} else {
-				readLimit = req.Limit
-			}
-
-			if readLimit == 0 {
-				if err := stream.Send(response); err != nil {
-					return status.Errorf(codes.Unavailable, "GetBlobs: send: %v", err)
-				}
-			} else {
-				sw := streamio.NewWriter(func(p []byte) error {
-					msg := &pb.GetBlobsResponse{}
-					if response != nil {
-						msg = response
-						response = nil
-					}
-
-					msg.Data = p
-
-					return stream.Send(msg)
-				})
-
-				sent, err := io.Copy(sw, io.LimitReader(stdout, readLimit))
-				if err != nil {
-					return status.Errorf(codes.Unavailable, "GetBlobs: send: %v", err)
-				}
-				if sent != readLimit {
-					return status.Errorf(codes.Unavailable, "GetBlobs: short send: %d/%d bytes", sent, objectInfo.Size)
-				}
-			}
-
-			discardSize := int64(1) // A new line after the blob content
-			if readLimit < objectInfo.Size {
-				discardSize += objectInfo.Size - readLimit
-			}
-
-			discarded, err := io.Copy(ioutil.Discard, io.LimitReader(stdout, discardSize))
-			if err != nil {
-				return status.Errorf(codes.Unavailable, "GetBlobs: discarding data: %v", err)
-			}
-			if discarded != discardSize {
-				return status.Errorf(codes.Unavailable, "GetBlobs: short discard: %d/%d bytes", discarded, discardSize)
+			if sent != readLimit {
+				return status.Errorf(codes.Unavailable, "GetBlobs: short send: %d/%d bytes", sent, objectInfo.Size)
 			}
 		}
 
-		return nil
+		discardSize := int64(0)
+		if readLimit < objectInfo.Size {
+			discardSize += objectInfo.Size - readLimit
+		}
+
+		discarded, err := io.Copy(ioutil.Discard, io.LimitReader(blobReader, discardSize))
+		if err != nil {
+			return status.Errorf(codes.Unavailable, "GetBlobs: discarding data: %v", err)
+		}
+		if discarded != discardSize {
+			return status.Errorf(codes.Unavailable, "GetBlobs: short discard: %d/%d bytes", discarded, discardSize)
+		}
 	}
+
+	return nil
 }
 
 func (*server) GetBlobs(req *pb.GetBlobsRequest, stream pb.BlobService_GetBlobsServer) error {
@@ -126,6 +125,5 @@ func (*server) GetBlobs(req *pb.GetBlobsRequest, stream pb.BlobService_GetBlobsS
 		return err
 	}
 
-	handler := getBlobsHandler(req, stream, c)
-	return catfile.CatFile(stream.Context(), req.Repository, handler)
+	return sendGetBlobsResponse(req, stream, c)
 }
