7a7bf4c9eef4ac9a7ad3e21937743fe80c05ad31
Jacob Vosmaer <jacob@gitlab.com>
2018-03-21 19:10:17 +0100

    Add comment about concurrency

--- internal/git/catfile/catfile.go
+++ internal/git/catfile/catfile.go
@@ -11,6 +11,10 @@ import (
 
 // C abstracts 'git cat-file --batch' and 'git cat-file --batch-check'.
 // It lets you retrieve object metadata and raw objects from a Git repo.
+//
+// Although it's thread-safe in the sense of not panicking, a C instance
+// can only serve single request at a time. If you try to use it across
+// multiple goroutines you are likely to encounter errors.
 type C struct {
 	*batchCheck
 	*batch
