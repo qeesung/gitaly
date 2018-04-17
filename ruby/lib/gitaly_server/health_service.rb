826837a2d801e4d9d19461ea3e4393a77020e690
Jacob Vosmaer <jacob@gitlab.com>
2018-04-17 13:48:21 +0200

    whitespace

--- ruby/lib/gitaly_server/health_service.rb
+++ ruby/lib/gitaly_server/health_service.rb
@@ -2,7 +2,6 @@ require 'grpc/health/v1/health_services_pb'
 
 module GitalyServer
   class HealthService < Grpc::Health::V1::Health::Service
-
     def check(req, call)
       Grpc::Health::V1::HealthCheckResponse.new
     end
