245255b2c466c82b5023e7074059575a2e4a924e
Jacob Vosmaer <jacob@gitlab.com>
2018-07-03 18:01:25 +0200

    Rewrite ListCommitsByOid in Go

--- internal/service/commit/list_commits_by_oid.go
+++ internal/service/commit/list_commits_by_oid.go
@@ -1,36 +1,41 @@
 package commit
 
 import (
-	"gitlab.com/gitlab-org/gitaly/internal/rubyserver"
-
 	pb "gitlab.com/gitlab-org/gitaly-proto/go"
+	"gitlab.com/gitlab-org/gitaly/internal/git/catfile"
+	gitlog "gitlab.com/gitlab-org/gitaly/internal/git/log"
 )
 
 func (s *server) ListCommitsByOid(in *pb.ListCommitsByOidRequest, stream pb.CommitService_ListCommitsByOidServer) error {
 	ctx := stream.Context()
 
-	client, err := s.CommitServiceClient(ctx)
+	c, err := catfile.New(ctx, in.Repository)
 	if err != nil {
 		return err
 	}
 
-	clientCtx, err := rubyserver.SetHeaders(ctx, in.GetRepository())
-	if err != nil {
-		return err
-	}
-
-	rubyStream, err := client.ListCommitsByOid(clientCtx, in)
-	if err != nil {
-		return err
-	}
-
-	return rubyserver.Proxy(func() error {
-		resp, err := rubyStream.Recv()
+	var commits []*pb.GitCommit
+	for _, oid := range in.Oid {
+		commit, err := gitlog.GetCommitCatfile(c, oid)
 		if err != nil {
-			md := rubyStream.Trailer()
-			stream.SetTrailer(md)
 			return err
 		}
-		return stream.Send(resp)
-	})
+
+		if commit != nil {
+			commits = append(commits, commit)
+		}
+
+		if len(commits) == 20 {
+			if err := stream.Send(&pb.ListCommitsByOidResponse{Commits: commits}); err != nil {
+				return err
+			}
+			commits = nil
+		}
+	}
+
+	if len(commits) > 0 {
+		return stream.Send(&pb.ListCommitsByOidResponse{Commits: commits})
+	}
+
+	return nil
 }
