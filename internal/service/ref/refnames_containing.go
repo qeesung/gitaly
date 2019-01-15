a94db5011fe36e810efc9a14621e629fd8891d38
Jacob Vosmaer <jacob@gitlab.com>
2019-01-15 13:08:20 +0100

    Refactor refnames RPC's to use chunker

---
+++ internal/service/ref/refnames_containing.go
@@ -0,0 +1,90 @@
+package ref
+
+import (
+	"bytes"
+	"fmt"
+
+	"gitlab.com/gitlab-org/gitaly-proto/go/gitalypb"
+	"gitlab.com/gitlab-org/gitaly/internal/git"
+	"gitlab.com/gitlab-org/gitaly/internal/helper"
+	"gitlab.com/gitlab-org/gitaly/internal/helper/chunker"
+)
+
+// ListBranchNamesContainingCommit returns a maximum of in.GetLimit() Branch names
+// which contain the SHA1 passed as argument
+func (*server) ListBranchNamesContainingCommit(in *gitalypb.ListBranchNamesContainingCommitRequest, stream gitalypb.RefService_ListBranchNamesContainingCommitServer) error {
+	if err := git.ValidateCommitID(in.GetCommitId()); err != nil {
+		return helper.ErrInvalidArgument(err)
+	}
+
+	if err := listBranchNamesContainingCommit(in, stream); err != nil {
+		return helper.ErrInternal(err)
+	}
+
+	return nil
+}
+
+func listBranchNamesContainingCommit(in *gitalypb.ListBranchNamesContainingCommitRequest, stream gitalypb.RefService_ListBranchNamesContainingCommitServer) error {
+	chunk := chunker.New(&branchNamesContainingCommitSender{stream: stream})
+	args := []string{fmt.Sprintf("--contains=%s", in.GetCommitId())}
+	if in.GetLimit() != 0 {
+		args = append(args, fmt.Sprintf("--count=%d", in.GetLimit()))
+	}
+
+	return listRefNames(stream.Context(), chunk, "refs/heads", in.Repository, args)
+}
+
+type branchNamesContainingCommitSender struct {
+	stream      gitalypb.RefService_ListBranchNamesContainingCommitServer
+	branchNames [][]byte
+}
+
+func (bs *branchNamesContainingCommitSender) Reset() { bs.branchNames = nil }
+func (bs *branchNamesContainingCommitSender) Append(it chunker.Item) {
+	bs.branchNames = append(bs.branchNames, stripPrefix(it, "refs/heads/"))
+}
+func (bs *branchNamesContainingCommitSender) Send() error {
+	return bs.stream.Send(&gitalypb.ListBranchNamesContainingCommitResponse{BranchNames: bs.branchNames})
+}
+
+// ListTagNamesContainingCommit returns a maximum of in.GetLimit() Tag names
+// which contain the SHA1 passed as argument
+func (*server) ListTagNamesContainingCommit(in *gitalypb.ListTagNamesContainingCommitRequest, stream gitalypb.RefService_ListTagNamesContainingCommitServer) error {
+	if err := git.ValidateCommitID(in.GetCommitId()); err != nil {
+		return helper.ErrInvalidArgument(err)
+	}
+
+	if err := listTagNamesContainingCommit(in, stream); err != nil {
+		return helper.ErrInternal(err)
+	}
+
+	return nil
+}
+
+func listTagNamesContainingCommit(in *gitalypb.ListTagNamesContainingCommitRequest, stream gitalypb.RefService_ListTagNamesContainingCommitServer) error {
+	chunk := chunker.New(&tagNamesContainingCommitSender{stream: stream})
+
+	args := []string{fmt.Sprintf("--contains=%s", in.GetCommitId())}
+	if in.GetLimit() != 0 {
+		args = append(args, fmt.Sprintf("--count=%d", in.GetLimit()))
+	}
+
+	return listRefNames(stream.Context(), chunk, "refs/tags", in.Repository, args)
+}
+
+type tagNamesContainingCommitSender struct {
+	stream   gitalypb.RefService_ListTagNamesContainingCommitServer
+	tagNames [][]byte
+}
+
+func (ts *tagNamesContainingCommitSender) Reset() { ts.tagNames = nil }
+func (ts *tagNamesContainingCommitSender) Append(it chunker.Item) {
+	ts.tagNames = append(ts.tagNames, stripPrefix(it, "refs/tags/"))
+}
+func (ts *tagNamesContainingCommitSender) Send() error {
+	return ts.stream.Send(&gitalypb.ListTagNamesContainingCommitResponse{TagNames: ts.tagNames})
+}
+
+func stripPrefix(it chunker.Item, prefix string) []byte {
+	return bytes.TrimPrefix(it.([]byte), []byte(prefix))
+}
