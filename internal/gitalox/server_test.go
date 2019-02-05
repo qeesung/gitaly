4f1ee574a9aa6f1745485a1dddb8052e830a6e46
Paul Okstad <pokstad@gitlab.com>
2019-02-04 22:21:16 -0800

    update test to include downstream server (#1472)

--- internal/gitalox/server_test.go
+++ internal/gitalox/server_test.go
@@ -2,19 +2,25 @@ package gitalox_test
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"testing"
+	"time"
 
+	"gitlab.com/gitlab-org/gitaly-proto/go/gitalypb"
+	"gitlab.com/gitlab-org/gitaly/client"
 	"gitlab.com/gitlab-org/gitaly/internal/gitalox"
+	"google.golang.org/grpc"
 )
 
 func TestServerRouting(t *testing.T) {
-	loxSrv := gitalox.NewServer(nil)
+	loxSrv := gitalox.NewServer(nil, testLogger{t})
 
 	listener, port := listenAvailPort(t)
 	t.Logf("proxy listening on port %d", port)
+	defer listener.Close()
 
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	errQ := make(chan error)
@@ -22,6 +28,25 @@ func TestServerRouting(t *testing.T) {
 	go func() {
 		errQ <- loxSrv.Start(ctx, listener)
 	}()
+
+	cc, err := dialLocalPort(t, port)
+	if err != nil {
+		t.Fatalf("client unable to dial to local port %d: %s", port, err)
+	}
+	defer cc.Close()
+
+	mCli, _, cleanup := newMockDownstream(t)
+	defer cleanup() // clean up mock downstream server resources
+
+	loxSrv.RegisterNode("test", mCli)
+
+	gCli := gitalypb.NewRepositoryServiceClient(cc)
+	resp, err := gCli.CalculateChecksum(ctx, &gitalypb.CalculateChecksumRequest{})
+	if err != nil {
+		t.Fatalf("unable to invoke RPC on proxy downstream: %s", err)
+	}
+
+	t.Logf("CalculateChecksum response: %#v", resp)
 
 	cancel()
 	t.Logf("Server teminated: %s", <-errQ)
@@ -34,4 +59,49 @@ func listenAvailPort(tb testing.TB) (net.Listener, int) {
 	}
 
 	return listener, listener.Addr().(*net.TCPAddr).Port
+}
+
+func dialLocalPort(tb testing.TB, port int) (*grpc.ClientConn, error) {
+	return client.Dial(
+		fmt.Sprintf("tcp://localhost:%d", port),
+		[]grpc.DialOption{grpc.WithBlock()},
+	)
+}
+
+type testLogger struct {
+	testing.TB
+}
+
+func (tl testLogger) Debugf(format string, args ...interface{}) {
+	tl.TB.Logf(format, args...)
+}
+
+// initializes and returns a client to downstream server, downstream server, and cleanup function
+func newMockDownstream(tb testing.TB) (*grpc.ClientConn, gitalypb.RepositoryServiceServer, func()) {
+	// setup mock server
+	m := &mockDownstreamRepoSvc{
+		srv: grpc.NewServer(),
+	}
+	gitalypb.RegisterRepositoryServiceServer(m.srv, m)
+	lis, port := listenAvailPort(tb)
+
+	// set up client to mock server
+	cc, err := dialLocalPort(tb, port)
+	if err != nil {
+		tb.Fatalf("unable to dial to mock downstream: %s", err)
+	}
+
+	cleanup := func() {
+		lis.Close()
+		m.srv.GracefulStop()
+		cc.Close()
+	}
+
+	errQ := make(chan error)
+
+	go func() {
+		errQ <- m.srv.Serve(lis)
+	}()
+
+	return cc, m, cleanup
 }
