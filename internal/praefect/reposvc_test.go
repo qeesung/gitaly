8af9af2344c0d850c5e345384d9676f7ef8de3e6
Paul Okstad <pokstad@gitlab.com>
2019-02-27 00:26:33 -0800

    simplified and more comprehensive prafect test

--- internal/praefect/reposvc_test.go
+++ internal/praefect/reposvc_test.go
@@ -3,148 +3,22 @@ package praefect_test
 import (
 	"context"
 
-	"gitlab.com/gitlab-org/gitaly-proto/go/gitalypb"
-	"google.golang.org/grpc"
+	"gitlab.com/gitlab-org/gitaly/internal/praefect/mock"
 )
 
-// mockRepoSvc is a mock implementation of gitalypb.RepositoryServiceServer
-// for testing purposes
-type mockRepoSvc struct {
-	srv *grpc.Server
+type simpleUnaryUnaryCallback func(context.Context, *mock.SimpleRequest) (*mock.SimpleResponse, error)
+
+// mockSvc is an implementation of mock.SimpleServer for testing purposes. The
+// gRPC stub can be updated via go generate:
+//
+//go:generate protoc --go_out=plugins=grpc:. mock/mock.proto
+type mockSvc struct {
+
+	// fields starting with 'm' allow us to change the RPC method behavior
+	simpleUnaryUnary simpleUnaryUnaryCallback
 }
 
-func (m *mockRepoSvc) RepositoryExists(context.Context, *gitalypb.RepositoryExistsRequest) (*gitalypb.RepositoryExistsResponse, error) {
-	return &gitalypb.RepositoryExistsResponse{}, nil
-}
-
-func (m *mockRepoSvc) RepackIncremental(context.Context, *gitalypb.RepackIncrementalRequest) (*gitalypb.RepackIncrementalResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) RepackFull(context.Context, *gitalypb.RepackFullRequest) (*gitalypb.RepackFullResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) GarbageCollect(context.Context, *gitalypb.GarbageCollectRequest) (*gitalypb.GarbageCollectResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) RepositorySize(context.Context, *gitalypb.RepositorySizeRequest) (*gitalypb.RepositorySizeResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) ApplyGitattributes(context.Context, *gitalypb.ApplyGitattributesRequest) (*gitalypb.ApplyGitattributesResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) FetchRemote(context.Context, *gitalypb.FetchRemoteRequest) (*gitalypb.FetchRemoteResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) CreateRepository(context.Context, *gitalypb.CreateRepositoryRequest) (*gitalypb.CreateRepositoryResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) GetArchive(*gitalypb.GetArchiveRequest, gitalypb.RepositoryService_GetArchiveServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) HasLocalBranches(context.Context, *gitalypb.HasLocalBranchesRequest) (*gitalypb.HasLocalBranchesResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) FetchSourceBranch(context.Context, *gitalypb.FetchSourceBranchRequest) (*gitalypb.FetchSourceBranchResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) Fsck(context.Context, *gitalypb.FsckRequest) (*gitalypb.FsckResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) WriteRef(context.Context, *gitalypb.WriteRefRequest) (*gitalypb.WriteRefResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) FindMergeBase(context.Context, *gitalypb.FindMergeBaseRequest) (*gitalypb.FindMergeBaseResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) CreateFork(context.Context, *gitalypb.CreateForkRequest) (*gitalypb.CreateForkResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) IsRebaseInProgress(context.Context, *gitalypb.IsRebaseInProgressRequest) (*gitalypb.IsRebaseInProgressResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) IsSquashInProgress(context.Context, *gitalypb.IsSquashInProgressRequest) (*gitalypb.IsSquashInProgressResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) CreateRepositoryFromURL(context.Context, *gitalypb.CreateRepositoryFromURLRequest) (*gitalypb.CreateRepositoryFromURLResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) CreateBundle(*gitalypb.CreateBundleRequest, gitalypb.RepositoryService_CreateBundleServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) CreateRepositoryFromBundle(gitalypb.RepositoryService_CreateRepositoryFromBundleServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) WriteConfig(context.Context, *gitalypb.WriteConfigRequest) (*gitalypb.WriteConfigResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) SetConfig(context.Context, *gitalypb.SetConfigRequest) (*gitalypb.SetConfigResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) DeleteConfig(context.Context, *gitalypb.DeleteConfigRequest) (*gitalypb.DeleteConfigResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) FindLicense(context.Context, *gitalypb.FindLicenseRequest) (*gitalypb.FindLicenseResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) GetInfoAttributes(*gitalypb.GetInfoAttributesRequest, gitalypb.RepositoryService_GetInfoAttributesServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) CalculateChecksum(context.Context, *gitalypb.CalculateChecksumRequest) (*gitalypb.CalculateChecksumResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) Cleanup(context.Context, *gitalypb.CleanupRequest) (*gitalypb.CleanupResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) GetSnapshot(*gitalypb.GetSnapshotRequest, gitalypb.RepositoryService_GetSnapshotServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) CreateRepositoryFromSnapshot(context.Context, *gitalypb.CreateRepositoryFromSnapshotRequest) (*gitalypb.CreateRepositoryFromSnapshotResponse, error) {
-	return nil, nil
-}
-
-func (m *mockRepoSvc) GetRawChanges(*gitalypb.GetRawChangesRequest, gitalypb.RepositoryService_GetRawChangesServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) SearchFilesByContent(*gitalypb.SearchFilesByContentRequest, gitalypb.RepositoryService_SearchFilesByContentServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) SearchFilesByName(*gitalypb.SearchFilesByNameRequest, gitalypb.RepositoryService_SearchFilesByNameServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) RestoreCustomHooks(gitalypb.RepositoryService_RestoreCustomHooksServer) error {
-	return nil
-}
-
-func (m *mockRepoSvc) BackupCustomHooks(*gitalypb.BackupCustomHooksRequest, gitalypb.RepositoryService_BackupCustomHooksServer) error {
-	return nil
+// SimpleUnaryUnary is implemented by a callback
+func (m *mockSvc) SimpleUnaryUnary(ctx context.Context, req *mock.SimpleRequest) (*mock.SimpleResponse, error) {
+	return m.simpleUnaryUnary(ctx, req)
 }
--- internal/praefect/server_test.go
+++ internal/praefect/server_test.go
@@ -9,44 +9,80 @@ import (
 
 	"github.com/mwitkow/grpc-proxy/proxy"
 	"github.com/stretchr/testify/require"
-	"gitlab.com/gitlab-org/gitaly-proto/go/gitalypb"
 	"gitlab.com/gitlab-org/gitaly/client"
 	"gitlab.com/gitlab-org/gitaly/internal/praefect"
+	"gitlab.com/gitlab-org/gitaly/internal/praefect/mock"
 	"google.golang.org/grpc"
 )
 
-func TestServerRouting(t *testing.T) {
-	prf := praefect.NewServer(nil, testLogger{t})
+// TestServerSimpleUnaryUnary verifies that the Praefect server is capable of
+// routing a specific unary request to and unary response from a backend server
+func TestServerSimpleUnaryUnary(t *testing.T) {
+	testCases := []struct {
+		name string
 
-	listener, port := listenAvailPort(t)
-	t.Logf("proxy listening on port %d", port)
-	defer listener.Close()
+		// callback is the actual RPC implementation
+		callback simpleUnaryUnaryCallback
 
-	errQ := make(chan error)
+		// all inputs and outputs for RPC SimpleUnaryUnary
+		request    *mock.SimpleRequest
+		expectResp *mock.SimpleResponse
+		expectErr  error
+	}{
+		{
+			name:     "simple request with response",
+			callback: callbackIncrement,
+			request: &mock.SimpleRequest{
+				Value: 1,
+			},
+			expectResp: &mock.SimpleResponse{
+				Value: 2,
+			},
+		},
+	}
 
-	go func() {
-		errQ <- prf.Start(listener)
-	}()
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			prf := praefect.NewServer(nil, testLogger{t})
 
-	// dial client to proxy
-	cc := dialLocalPort(t, port, false)
-	defer cc.Close()
-	gCli := gitalypb.NewRepositoryServiceClient(cc)
+			listener, port := listenAvailPort(t)
+			t.Logf("proxy listening on port %d", port)
+			defer listener.Close()
 
-	mCli, _, cleanup := newMockDownstream(t)
-	defer cleanup() // clean up mock downstream server resources
+			errQ := make(chan error)
 
-	prf.RegisterNode("test", mCli)
+			go func() {
+				errQ <- prf.Start(listener)
+			}()
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+			// dial client to praefect
+			cc := dialLocalPort(t, port, false)
+			defer cc.Close()
+			cli := mock.NewSimpleServiceClient(cc)
 
-	_, err := gCli.RepositoryExists(ctx, &gitalypb.RepositoryExistsRequest{})
-	require.NoError(t, err)
+			backend, cleanup := newMockDownstream(t, tt.callback)
+			defer cleanup() // clean up mock downstream server resources
 
-	err = prf.Shutdown(ctx)
-	require.NoError(t, err)
-	require.NoError(t, <-errQ)
+			prf.RegisterNode("test", backend)
+
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			resp, err := cli.SimpleUnaryUnary(ctx, tt.request)
+			require.Equal(t, tt.expectResp, resp)
+			require.Equal(t, tt.expectErr, err)
+
+			err = prf.Shutdown(ctx)
+			require.NoError(t, err)
+			require.NoError(t, <-errQ)
+		})
+	}
+}
+
+func callbackIncrement(_ context.Context, req *mock.SimpleRequest) (*mock.SimpleResponse, error) {
+	return &mock.SimpleResponse{
+		Value: req.Value + 1,
+	}, nil
 }
 
 func listenAvailPort(tb testing.TB) (net.Listener, int) {
@@ -85,29 +121,31 @@ func (tl testLogger) Debugf(format string, args ...interface{}) {
 }
 
 // initializes and returns a client to downstream server, downstream server, and cleanup function
-func newMockDownstream(tb testing.TB) (*grpc.ClientConn, gitalypb.RepositoryServiceServer, func()) {
+func newMockDownstream(tb testing.TB, callback simpleUnaryUnaryCallback) (*grpc.ClientConn, func()) {
 	// setup mock server
-	m := &mockRepoSvc{
-		srv: grpc.NewServer(),
+	m := &mockSvc{
+		simpleUnaryUnary: callback,
 	}
-	gitalypb.RegisterRepositoryServiceServer(m.srv, m)
-	lis, port := listenAvailPort(tb)
 
-	// dial praefect to backend service
+	srv := grpc.NewServer()
+	mock.RegisterSimpleServiceServer(srv, m)
+
+	// client to backend service
+	lis, port := listenAvailPort(tb)
 	cc := dialLocalPort(tb, port, true)
 
 	errQ := make(chan error)
 
 	go func() {
-		errQ <- m.srv.Serve(lis)
+		errQ <- srv.Serve(lis)
 	}()
 
 	cleanup := func() {
-		m.srv.GracefulStop()
+		srv.GracefulStop()
 		lis.Close()
 		cc.Close()
 		require.NoError(tb, <-errQ)
 	}
 
-	return cc, m, cleanup
+	return cc, cleanup
 }
