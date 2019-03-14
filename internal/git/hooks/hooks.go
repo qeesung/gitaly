035e4c2ee2556b0865f93eb9d6e98755f4e3aa70
Jacob Vosmaer <jacob@gitlab.com>
2019-03-14 11:46:43 +0100

    update comment

--- internal/git/hooks/hooks.go
+++ internal/git/hooks/hooks.go
@@ -7,12 +7,13 @@ import (
 	"gitlab.com/gitlab-org/gitaly/internal/config"
 )
 
-// Override allows tests to control where the hooks directory is. Outside
-// consumers can also use the GITALY_TESTING_NO_GIT_HOOKS environment
-// variable.
+// Override allows tests to control where the hooks directory is.
 var Override string
 
-// Path returns the path where the global git hooks are located.
+// Path returns the path where the global git hooks are located. If the
+// environment variable GITALY_TESTING_NO_GIT_HOOKS is set to "1", Path
+// will return an empty directory, which has the effect that no Git hooks
+// will run at all.
 func Path() string {
 	if len(Override) > 0 {
 		return Override
