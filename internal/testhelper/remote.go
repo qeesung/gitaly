c4c7faf2a41f455927b3923ba2b2608bed3c66da
Jacob Vosmaer <jacob@gitlab.com>
2019-03-26 11:54:49 +0100

    improve RemoteExists readability

--- internal/testhelper/remote.go
+++ internal/testhelper/remote.go
@@ -5,16 +5,15 @@ import (
 	"testing"
 )
 
-// RemoteExists tests if the repository at repoPath has are Git remote named remote.
-func RemoteExists(t *testing.T, repoPath string, remote string) bool {
+// RemoteExists tests if the repository at repoPath has are Git remote named remoteName.
+func RemoteExists(t *testing.T, repoPath string, remoteName string) bool {
 	if len(remote) == 0 {
 		t.Fatalf("empty remote name")
 	}
 
 	remotes := MustRunCommand(t, nil, "git", "-C", repoPath, "remote")
-
 	for _, r := range strings.Split(string(remotes), "\n") {
-		if r == remote {
+		if r == remoteName {
 			return true
 		}
 	}
