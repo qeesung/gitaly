5e61ff64dd1a7f20ca00d083dcea89872f1b24c2
Paul Okstad <pokstad@gitlab.com>
2019-04-22 14:03:37 -0700

    test helper for reading config files

---
+++ internal/testhelper/config_test.go
@@ -0,0 +1,38 @@
+package testhelper_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
+)
+
+var cfgData = `
+[core]
+  bare = false
+	commitGraph = true
+[remote "origin"]
+	url = git@gitlab.com:gitlab-org/gitaly.git
+	fetch = +refs/heads/*:refs/remotes/origin/*
+`
+
+func TestConfigParser(t *testing.T) {
+	cfg, err := testhelper.ParseConfig(bytes.NewBuffer([]byte(cfgData)))
+	require.NoError(t, err)
+
+	for _, tc := range []struct {
+		section     string
+		key         string
+		expectValue string
+	}{
+		{"core", "commitGraph", "true"},
+		{"core", "bare", "false"},
+		{`remote "origin"`, "url", "git@gitlab.com:gitlab-org/gitaly.git"},
+		{`remote "origin"`, "fetch", "+refs/heads/*:refs/remotes/origin/*"},
+	} {
+		actualValue, ok := cfg.Section(tc.section, tc.key)
+		require.True(t, ok)
+		require.Equal(t, tc.expectValue, actualValue)
+	}
+}
