cc1ea8ce6a3aac871ef9ad2e554b1d50bf9c8615
Michael Hofmann <michael.hofmann@bitgestalt.com>
2019-07-03 10:32:12 +0200

    Use only one public FileSystem function.

--- internal/helper/fstype/detect_unix.go
+++ internal/helper/fstype/detect_unix.go
@@ -4,8 +4,7 @@ package fstype
 
 import "golang.org/x/sys/unix"
 
-// FileSystem will return the type of filesystem being used at the passed path
-func FileSystem(path string) string {
+func detectFileSystem(path string) string {
 	var stat unix.Statfs_t
 	if err := unix.Statfs(path, &stat); err != nil {
 		return unknownFS
