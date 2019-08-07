dc281fc3fb425837e90ccdc22854db27710bcc12
Jacob Vosmaer <jacob@gitlab.com>
2019-08-07 19:21:02 +0200

    Use branch

--- internal/service/operations/squash_test.go
+++ internal/service/operations/squash_test.go
@@ -3,7 +3,6 @@ package operations
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -91,38 +90,11 @@ func TestSuccessfulUserSquashRequestWith3wayMerge(t *testing.T) {
 	testRepo, testRepoPath, cleanupFn := testhelper.NewTestRepo(t)
 	defer cleanupFn()
 
-	// The following patch inserts a single line so that we create a
-	// file that is slightly different from a branched version of this
-	// file.  A 3-way merge must be used in order to merge a branch that
-	// alters lines involved in this diff.
-
-	// Create blob ff8539473110911d91a58d48df9c18b6d940d290
-	blobPath, err := filepath.Abs("testdata/popen-blob.txt")
-	require.NoError(t, err)
-	blobOID := testhelper.MustRunCommand(t, nil, "git", "-C", testRepoPath, "hash-object", "-w", blobPath)
-	require.Equal(t, "ff8539473110911d91a58d48df9c18b6d940d290", text.ChompBytes(blobOID))
-
-	// testdata/3way-trees.txt defines trees that bring in blob
-	// ff8539473110911d91a58d48df9c18b6d940d290 (created above) as the new
-	// contents of files/ruby/popen.rb.
-	treeSpec, err := os.Open("testdata/3way-trees.txt")
-	require.NoError(t, err)
-	defer treeSpec.Close()
-	mktree := testhelper.MustRunCommand(t, treeSpec, "git", "-C", testRepoPath, "mktree", "--batch")
-
-	// This is the OID of the root tree we just created with testdata/3way-trees.txt.
-	treeRootOID := "d43533ff663c4181006d1319210236f525f44381"
-	expectedTrees := fmt.Sprintf("61d929f8f1f9ffbbf2db23f04d6fb50b0283f1ab\n2e147c212184f0c5a8eca678114e0b8bb61156fc\n%s\n", treeRootOID)
-	require.Equal(t, expectedTrees, string(mktree))
-
-	baseSha := text.ChompBytes(testhelper.MustRunCommand(t, nil, "git", "-C", testRepoPath, "commit-tree", "-p", "v1.0.0^{commit}", "-m", "msg", treeRootOID))
-	testhelper.MustRunCommand(t, nil, "git", "-C", testRepoPath, "update-ref", "refs/heads/3way-test", baseSha)
-
 	request := &gitalypb.UserSquashRequest{
 		Repository:    testRepo,
 		User:          user,
 		SquashId:      "1",
-		Branch:        []byte("3way-test"),
+		Branch:        []byte("gitaly/squash-test"),
 		Author:        author,
 		CommitMessage: commitMessage,
 		// The diff between two of these commits results in some changes to files/ruby/popen.rb
@@ -136,7 +108,7 @@ func TestSuccessfulUserSquashRequestWith3wayMerge(t *testing.T) {
 
 	commit, err := log.GetCommit(ctx, testRepo, response.SquashSha)
 	require.NoError(t, err)
-	require.Equal(t, []string{baseSha}, commit.ParentIds)
+	require.Equal(t, []string{"3d05a143ac193c1a6fe4d046a6e3fe71e825258a"}, commit.ParentIds)
 	require.Equal(t, author.Name, commit.Author.Name)
 	require.Equal(t, author.Email, commit.Author.Email)
 	require.Equal(t, user.Name, commit.Committer.Name)
