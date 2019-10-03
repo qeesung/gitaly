7cb1c37ace09d53824560e0c82d6841e103e2b8e
Jacob Vosmaer <jacob@gitlab.com>
2019-10-03 16:55:42 +0200

    Add counter

--- internal/git/bitmap.go
+++ internal/git/bitmap.go
@@ -3,11 +3,24 @@ package git
 import (
 	"context"
 	"os"
+	"strconv"
 	"strings"
 
 	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"github.com/prometheus/client_golang/prometheus"
 	"gitlab.com/gitlab-org/gitaly/internal/git/packfile"
 )
+
+var badBitmapRequestCount = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "gitaly_bad_bitmap_request_total",
+		Help: "RPC calls during which there was not exactly 1 packfile bitmap",
+	},
+	[]string{"method", "bitmaps"},
+)
+
+func init() { prometheus.MustRegister(badBitmapRequestCount) }
 
 // WarnIfTooManyBitmaps checks for too many (more than one) bitmaps in
 // repoPath, and if it finds any, it logs a warning. This is to help us
@@ -43,9 +56,17 @@ func WarnIfTooManyBitmaps(ctx context.Context, repoPath string) {
 		}
 	}
 
-	if count == 0 {
+	if count == 1 {
 		return
 	}
 
-	logEntry.WithField("bitmaps", count).Warn("found more than one packfile bitmap in repository")
+	if count > 1 {
+		logEntry.WithField("bitmaps", count).Warn("found more than one packfile bitmap in repository")
+	}
+
+	grpcMethod, ok := grpc_ctxtags.Extract(ctx).Values()["grpc.request.fullMethod"].(string)
+	if !ok {
+		return
+	}
+	badBitmapRequestCount.WithLabelValues(grpcMethod, strconv.Itoa(count)).Inc()
 }
