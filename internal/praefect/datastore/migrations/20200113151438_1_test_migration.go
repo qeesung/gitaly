d1d2773e7a92cb9ff42a19c17b0a27ccf7bb679c
Jacob Vosmaer <jacob@gitlab.com>
2020-01-13 16:15:22 +0100

    Rename test migration

--- internal/praefect/datastore/migrations/20200113151438_1_test_migration.go
+++ internal/praefect/datastore/migrations/20200113151438_1_test_migration.go
@@ -4,7 +4,7 @@ import migrate "github.com/rubenv/sql-migrate"
 
 func init() {
 	m := &migrate.Migration{
-		Id:   "20200109161456_aaa",
+		Id:   "20200113151438_1_test_migration",
 		Up:   []string{"INSERT INTO hello_world (id) VALUES (1)"},
 		Down: []string{"DELETE FROM hello_world WHERE id = 1"},
 	}
