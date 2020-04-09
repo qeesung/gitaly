2b566ad250a2068366227805001dc52b7262cad0
Paul Okstad <pokstad@gitlab.com>
2020-04-09 09:57:09 -0700

    Remove feature flags for InfoRef cache

--- internal/service/smarthttp/cache.go
+++ internal/service/smarthttp/cache.go
@@ -45,12 +45,8 @@ func init() {
 	prometheus.MustRegister(hitMissTotals)
 }
 
-// UploadPackCacheFeatureFlagKey enables cache usage in InfoRefsUploadPack RPC
-const UploadPackCacheFeatureFlagKey = "inforef-uploadpack-cache"
-
 func tryCache(ctx context.Context, in *gitalypb.InfoRefsRequest, w io.Writer, missFn func(io.Writer) error) error {
-	if !featureflag.IsEnabled(ctx, UploadPackCacheFeatureFlagKey) ||
-		!featureflag.IsEnabled(ctx, featureflag.CacheInvalidator) ||
+	if featureflag.IsEnabled(ctx, featureflag.UploadPackFilter) ||
 		len(in.GetGitConfigOptions()) > 0 ||
 		len(in.GetGitProtocol()) > 0 {
 		return missFn(w)
