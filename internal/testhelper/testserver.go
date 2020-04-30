834d789c44778f40ade04d9dc1f58e95196e8c80
Pavlo Strokov <pstrokov@gitlab.com>
2020-04-30 15:23:29 +0300

    Praefect: proper multi-virtual storage support
    Make routing dependent on the virtual storage passed and config
    provided for this virtual storage.
    Replication job includes virtual storage info to be properly
    routed when applied to the target storage (support of same storage
    names for different virtual storages).

--- internal/testhelper/testserver.go
+++ internal/testhelper/testserver.go
@@ -126,17 +126,16 @@ func (p *TestServer) Socket() string {
 
 // Start will start the grpc server as well as spawn a praefect instance if GITALY_TEST_PRAEFECT_BIN is enabled
 func (p *TestServer) Start() error {
-	gitalyServerSocketPath := GetTemporaryGitalySocketFileName()
-
-	listener, err := net.Listen("unix", gitalyServerSocketPath)
-	if err != nil {
-		return err
-	}
-
-	go p.grpcServer.Serve(listener)
-
 	praefectBinPath, ok := os.LookupEnv("GITALY_TEST_PRAEFECT_BIN")
 	if !ok {
+		gitalyServerSocketPath := GetTemporaryGitalySocketFileName()
+		listener, err := net.Listen("unix", gitalyServerSocketPath)
+		if err != nil {
+			return err
+		}
+
+		go p.grpcServer.Serve(listener)
+
 		p.socket = gitalyServerSocketPath
 		return nil
 	}
@@ -164,6 +163,15 @@ func (p *TestServer) Start() error {
 	}
 
 	for _, storage := range p.storages {
+		gitalyServerSocketPath := GetTemporaryGitalySocketFileName()
+
+		listener, err := net.Listen("unix", gitalyServerSocketPath)
+		if err != nil {
+			return err
+		}
+
+		go p.grpcServer.Serve(listener)
+
 		c.VirtualStorages = append(c.VirtualStorages, &praefectconfig.VirtualStorage{
 			Name: storage,
 			Nodes: []*models.Node{
