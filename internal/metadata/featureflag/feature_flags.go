bf5d1faffe7422a6dbe515e56212b81ffbd517b0
John Cai <jcai@gitlab.com>
2020-07-22 14:04:38 -0700

    Fix feature sets

--- internal/metadata/featureflag/feature_flags.go
+++ internal/metadata/featureflag/feature_flags.go
@@ -1,31 +1,55 @@
 package featureflag
 
-type FeatureFlag struct {
-	Name        string `json:"name"`
-	OnByDefault bool   `json:"on_by_default"`
+type FeatureFlag interface {
+	Name() string
+	OnByDefault() bool
+}
+
+type RubyFeatureFlag struct {
+	GoFeatureFlag
+}
+
+type GoFeatureFlag struct {
+	name        string `json:"name"`
+	onByDefault bool   `json:"on_by_default"`
+}
+
+func (g GoFeatureFlag) Name() string {
+	return g.name
+}
+
+func (g GoFeatureFlag) OnByDefault() bool {
+	return g.onByDefault
+}
+
+func NewGoFeatureFlag(name string, onByDefault bool) *GoFeatureFlag {
+	return &GoFeatureFlag{
+		name:        name,
+		onByDefault: onByDefault,
+	}
 }
 
 var (
 	// GoUpdateHook will bypass the ruby update hook and use the go implementation of custom hooks
-	GoUpdateHook = FeatureFlag{Name: "go_update_hook", OnByDefault: true}
+	GoUpdateHook = GoFeatureFlag{name: "go_update_hook", onByDefault: true}
 	// RemoteBranchesLsRemote will use `ls-remote` for remote branches
-	RemoteBranchesLsRemote = FeatureFlag{Name: "ruby_remote_branches_ls_remote", OnByDefault: true}
+	RemoteBranchesLsRemote = GoFeatureFlag{name: "ruby_remote_branches_ls_remote", onByDefault: true}
 	// GoFetchSourceBranch enables a go implementation of FetchSourceBranch
-	GoFetchSourceBranch = FeatureFlag{Name: "go_fetch_source_branch", OnByDefault: false}
+	GoFetchSourceBranch = GoFeatureFlag{name: "go_fetch_source_branch", onByDefault: false}
 	// DistributedReads allows praefect to redirect accessor operations to up-to-date secondaries
-	DistributedReads = FeatureFlag{Name: "distributed_reads", OnByDefault: false}
+	DistributedReads = GoFeatureFlag{name: "distributed_reads", onByDefault: false}
 	// GoPreReceiveHook will bypass the ruby pre-receive hook and use the go implementation
-	GoPreReceiveHook = FeatureFlag{Name: "go_prereceive_hook", OnByDefault: true}
+	GoPreReceiveHook = GoFeatureFlag{name: "go_prereceive_hook", onByDefault: true}
 	// GoPostReceiveHook will bypass the ruby post-receive hook and use the go implementation
-	GoPostReceiveHook = FeatureFlag{Name: "go_postreceive_hook", OnByDefault: false}
+	GoPostReceiveHook = GoFeatureFlag{name: "go_postreceive_hook", onByDefault: false}
 	// ReferenceTransactions will handle Git reference updates via the transaction service for strong consistency
-	ReferenceTransactions = FeatureFlag{Name: "reference_transactions", OnByDefault: false}
+	ReferenceTransactions = GoFeatureFlag{name: "reference_transactions", onByDefault: false}
 	// ReferenceTransactionsOperationService will enable reference transactions for the OperationService
-	ReferenceTransactionsOperationService = FeatureFlag{Name: "reference_transactions_operation_service", OnByDefault: true}
+	ReferenceTransactionsOperationService = GoFeatureFlag{name: "reference_transactions_operation_service", onByDefault: true}
 	// ReferenceTransactionsSmartHTTPService will enable reference transactions for the SmartHTTPService
-	ReferenceTransactionsSmartHTTPService = FeatureFlag{Name: "reference_transactions_smarthttp_service", OnByDefault: true}
+	ReferenceTransactionsSmartHTTPService = GoFeatureFlag{name: "reference_transactions_smarthttp_service", onByDefault: true}
 	// ReferenceTransactionsSSHService will enable reference transactions for the SSHService
-	ReferenceTransactionsSSHService = FeatureFlag{Name: "reference_transactions_ssh_service", OnByDefault: true}
+	ReferenceTransactionsSSHService = GoFeatureFlag{name: "reference_transactions_ssh_service", onByDefault: true}
 )
 
 const (
