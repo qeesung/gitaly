a8697aa898a9e465a02146b4cc83b86b4b8a63c5
Jacob Vosmaer <jacob@gitlab.com>
2021-02-01 17:28:41 +0100

    Log cache key and response size

--- internal/gitaly/service/hook/pack_objects.go
+++ internal/gitaly/service/hook/pack_objects.go
@@ -1,10 +1,19 @@
 package hook
 
 import (
+	"crypto/sha256"
 	"errors"
+	"fmt"
+	"hash"
+	"io"
+	"io/ioutil"
+	"os"
 	"strings"
 	"sync"
 
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
+	"github.com/sirupsen/logrus"
 	"gitlab.com/gitlab-org/gitaly/internal/git"
 	"gitlab.com/gitlab-org/gitaly/internal/helper"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
@@ -34,15 +43,34 @@ func (s *server) PackObjectsHook(stream gitalypb.HookService_PackObjectsHookServ
 		return nil
 	}
 
-	stdin := streamio.NewReader(func() ([]byte, error) {
-		resp, err := stream.Recv()
-		return resp.GetStdin(), err
-	})
+	h := sha256.New()
+	if err := (&jsonpb.Marshaler{}).Marshal(h, firstRequest); err != nil {
+		return err
+	}
+
+	stdin, err := bufferStdin(stream, h)
+	if err != nil {
+		return err
+	}
+	defer stdin.Close()
+
+	key := h.Sum(nil)
+	var outBytes int64
+	logger := ctxlogrus.Extract(ctx)
+	defer func() {
+		logger.WithFields(logrus.Fields{
+			"cache_key": fmt.Sprintf("%x", key),
+			"out_bytes": outBytes,
+		}).Info("pack-objects")
+	}()
+
 	m := &sync.Mutex{}
 	stdout := streamio.NewSyncWriter(m, func(p []byte) error {
+		outBytes += int64(len(p))
 		return stream.Send(&gitalypb.PackObjectsHookResponse{Stdout: p})
 	})
 	stderr := streamio.NewSyncWriter(m, func(p []byte) error {
+		outBytes += int64(len(p))
 		return stream.Send(&gitalypb.PackObjectsHookResponse{Stderr: p})
 	})
 
@@ -105,4 +133,36 @@ func (p *packObjectsArgs) subcmd() git.SubCmd {
 		sc.Flags = append(sc.Flags, git.Flag{f})
 	}
 	return sc
+}
+
+func bufferStdin(stream gitalypb.HookService_PackObjectsHookServer, h hash.Hash) (r io.ReadCloser, err error) {
+	defer func() {
+		if r != nil && err != nil {
+			r.Close()
+		}
+	}()
+
+	stdin := streamio.NewReader(func() ([]byte, error) {
+		resp, err := stream.Recv()
+		return resp.GetStdin(), err
+	})
+
+	stdinTmp, err := ioutil.TempFile("", "PackObjectsHook")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := os.Remove(stdinTmp.Name()); err != nil {
+		return nil, err
+	}
+
+	if _, err := io.Copy(io.MultiWriter(h, stdinTmp), stdin); err != nil {
+		return nil, err
+	}
+
+	if _, err := stdinTmp.Seek(0, io.SeekStart); err != nil {
+		return nil, err
+	}
+
+	return stdinTmp, nil
 }
