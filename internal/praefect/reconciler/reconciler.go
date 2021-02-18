da90fc0901b6061002ee9c11f2fff111cbd91b26
Sami Hiltunen <shiltunen@gitlab.com>
2021-02-18 12:04:47 +0100

    do not use replicas which are pending deletion as replication source
    Praefect's reconciler is going to start scheduling jobs to delete
    unassigned repositories. With that in place, we should avoid using
    repositories that are scheduled for deletion as replication sources.
    This commit filters out up to date replicas from being used as a source
    if they have been scheduled for deletion.
    
    This should not generally happen anyway, given we only delete unassigned
    replicas if all assigned storages (replication targets) are up to date.
    This could happen if a storage is unassigned and rapidly another storage
    is assigned. The job would fail and a replication would get scheduled from
    another node, so this should not be a big problem but we may as well avoid
    it anyway.

--- internal/praefect/reconciler/reconciler.go
+++ internal/praefect/reconciler/reconciler.go
@@ -153,6 +153,15 @@ update_jobs AS (
 		FROM storage_repositories
 		JOIN healthy_storages USING (virtual_storage, storage)
 		JOIN repositories USING (virtual_storage, relative_path, generation)
+		WHERE NOT EXISTS (
+			SELECT true
+			FROM replication_queue
+			WHERE state NOT IN ('completed', 'dead', 'cancelled')
+			AND job->>'change' IN ('delete', 'delete_replica')
+			AND job->>'virtual_storage' = virtual_storage
+			AND job->>'relative_path' = relative_path
+			AND job->>'target_node_storage' = storage
+		)
 		ORDER BY virtual_storage, relative_path
 	) AS healthy_repositories USING (virtual_storage, relative_path)
 	WHERE NOT EXISTS (
