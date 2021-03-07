c118931824750ff2308272a873effc480b98d730
Pavlo Strokov <pstrokov@gitlab.com>
2021-03-07 14:52:38 +0200

    Configuration of the gitaly services dependencies
    Caller able to provide set of optional parameters to
    control dependencies used for the gitaly services creation.
    With those in place you can provide a mock implementation
    and assert on the call of the methods or use stubs.

--- internal/testhelper/testserver/gitaly.go
+++ internal/testhelper/testserver/gitaly.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"testing"
 
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/client"
 	"gitlab.com/gitlab-org/gitaly/internal/git"
@@ -13,6 +14,7 @@ import (
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/server"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/service"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/transaction"
+	"gitlab.com/gitlab-org/gitaly/internal/storage"
 	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
 	"google.golang.org/grpc"
 )
@@ -22,39 +24,108 @@ import (
 // It accepts addition Registrar to register all required service instead of
 // calling service.RegisterAll explicitly because it creates a circular dependency
 // when the function is used in on of internal/gitaly/service/... packages.
-func RunGitalyServer(t *testing.T, cfg config.Cfg, rubyServer *rubyserver.Server, registrar func(grpcServer *grpc.Server, deps *service.Dependencies)) (string, testhelper.Cleanup) {
+func RunGitalyServer(t *testing.T, cfg config.Cfg, rubyServer *rubyserver.Server, registrar func(srv *grpc.Server, deps *service.Dependencies), opts ...GitalyServerOpt) (string, testhelper.Cleanup) {
 	t.Helper()
 
-	conns := client.NewPool()
-	locator := config.NewLocator(cfg)
-	txManager := transaction.NewManager(cfg)
+	var deferrer testhelper.Deferrer
+	defer deferrer.Call()
 
-	srv, err := server.New(cfg.TLS.CertPath != "", cfg, testhelper.DiscardTestEntry(t))
+	var gsd gitalyServerDeps
+	for _, opt := range opts {
+		gsd = opt(gsd)
+	}
+
+	deps := gsd.createDependencies(t, cfg, rubyServer)
+	deferrer.Add(func() { gsd.conns.Close() })
+
+	srv, err := server.New(cfg.TLS.CertPath != "", cfg, gsd.logger.WithField("test", t.Name()))
 	require.NoError(t, err)
+	deferrer.Add(func() { srv.Stop() })
 
-	registrar(srv, &service.Dependencies{
-		Cfg:                cfg,
-		RubyServer:         rubyServer,
-		GitalyHookManager:  hook.NewManager(locator, txManager, hook.GitlabAPIStub, cfg),
-		TransactionManager: txManager,
-		StorageLocator:     locator,
-		ClientPool:         conns,
-		GitCmdFactory:      git.NewExecCommandFactory(cfg),
-	})
+	registrar(srv, deps)
 
+	// listen on internal socket
+	internalListener, err := net.Listen("unix", cfg.GitalyInternalSocketPath())
+	require.NoError(t, err)
+	deferrer.Add(func() { internalListener.Close() })
+	go srv.Serve(internalListener)
+
+	// listen on external socket
 	serverSocketPath := testhelper.GetTemporaryGitalySocketFileName(t)
 	listener, err := net.Listen("unix", serverSocketPath)
 	require.NoError(t, err)
-
-	//listen on internal socket
-	internalListener, err := net.Listen("unix", cfg.GitalyInternalSocketPath())
-	require.NoError(t, err)
-
-	go srv.Serve(internalListener)
+	deferrer.Add(func() { listener.Close() })
 	go srv.Serve(listener)
 
-	return "unix://" + serverSocketPath, func() {
-		conns.Close()
-		srv.Stop()
+	cleaner := deferrer.Relocate()
+	return "unix://" + serverSocketPath, cleaner.Call
+}
+
+type gitalyServerDeps struct {
+	logger        *logrus.Logger
+	conns         *client.Pool
+	locator       storage.Locator
+	txMgr         transaction.Manager
+	hookMgr       hook.Manager
+	gitlabAPI     hook.GitlabAPI
+	gitCmdFactory git.CommandFactory
+}
+
+func (gsd *gitalyServerDeps) createDependencies(t testing.TB, cfg config.Cfg, rubyServer *rubyserver.Server) *service.Dependencies {
+	if gsd.logger == nil {
+		gsd.logger = testhelper.DiscardTestLogger(t)
+	}
+
+	if gsd.conns == nil {
+		gsd.conns = client.NewPool()
+	}
+
+	if gsd.locator == nil {
+		gsd.locator = config.NewLocator(cfg)
+	}
+
+	if gsd.gitlabAPI == nil {
+		gsd.gitlabAPI = hook.GitlabAPIStub
+	}
+
+	if gsd.txMgr == nil {
+		gsd.txMgr = transaction.NewManager(cfg)
+	}
+
+	if gsd.hookMgr == nil {
+		gsd.hookMgr = hook.NewManager(gsd.locator, gsd.txMgr, gsd.gitlabAPI, cfg)
+	}
+
+	if gsd.gitCmdFactory == nil {
+		gsd.gitCmdFactory = git.NewExecCommandFactory(cfg)
+	}
+
+	return &service.Dependencies{
+		Cfg:                cfg,
+		RubyServer:         rubyServer,
+		ClientPool:         gsd.conns,
+		StorageLocator:     gsd.locator,
+		TransactionManager: gsd.txMgr,
+		GitalyHookManager:  gsd.hookMgr,
+		GitCmdFactory:      gsd.gitCmdFactory,
+	}
+}
+
+// GitalyServerOpt is a helper type to shorten declarations.
+type GitalyServerOpt func(gitalyServerDeps) gitalyServerDeps
+
+// WithLogger sets a logrus.Logger instance that will be used for gitaly services initialisation.
+func WithLogger(logger *logrus.Logger) GitalyServerOpt {
+	return func(deps gitalyServerDeps) gitalyServerDeps {
+		deps.logger = logger
+		return deps
+	}
+}
+
+// WithLocator sets a storage.Locator instance that will be used for gitaly services initialisation.
+func WithLocator(locator storage.Locator) GitalyServerOpt {
+	return func(deps gitalyServerDeps) gitalyServerDeps {
+		deps.locator = locator
+		return deps
 	}
 }
