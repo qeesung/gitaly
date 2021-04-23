99f7bf4cb448b88880197898460fa34a6f2fe1e7
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-04-23 12:32:10 +0200

    document stats

--- internal/command/stats.go
+++ internal/command/stats.go
@@ -9,11 +9,13 @@ import (
 
 type requestStatsKey struct{}
 
+// Stats hosts a map of statistics.
 type Stats struct {
 	registry map[string]int
 	sync.Mutex
 }
 
+// RecordSum adds the provided value to the value identified by key.
 func (stats *Stats) RecordSum(key string, value int) {
 	stats.Lock()
 	defer stats.Unlock()
@@ -25,6 +27,7 @@ func (stats *Stats) RecordSum(key string, value int) {
 	stats.registry[key] = value
 }
 
+// RecordMax updates the given key with the new value if it's greater than the current value.
 func (stats *Stats) RecordMax(key string, value int) {
 	stats.Lock()
 	defer stats.Unlock()
@@ -38,6 +41,7 @@ func (stats *Stats) RecordMax(key string, value int) {
 	stats.registry[key] = value
 }
 
+// Fields converts the given Stats structure into a set of logrus Fields for use in log messages.
 func (stats *Stats) Fields() logrus.Fields {
 	stats.Lock()
 	defer stats.Unlock()
@@ -49,11 +53,14 @@ func (stats *Stats) Fields() logrus.Fields {
 	return f
 }
 
+// StatsFromContext tries to extract stats from the provided context. It returns `nil` if tho
+// context does not have any stats.
 func StatsFromContext(ctx context.Context) *Stats {
 	stats, _ := ctx.Value(requestStatsKey{}).(*Stats)
 	return stats
 }
 
+// InitContextStats creates a new context with a Stats structure.
 func InitContextStats(ctx context.Context) context.Context {
 	return context.WithValue(ctx, requestStatsKey{}, &Stats{
 		registry: make(map[string]int),
