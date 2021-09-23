2c61b684745285dccd233956cd328744429fe1c5
Pavlo Strokov <pstrokov@gitlab.com>
2021-09-23 09:57:19 +0300

    memory: Pool of bytes buffers
    In order to reduce memory allocations and better utilise
    already allocated memory it makes sense to use a shared
    pool of *bytes.Buffer instances. The solution uses a standard
    sync.Pool that is used in stdlib for the same purpose.
    The Buffer() function acquires a new or existing instance
    from the pool and a release function that resets content
    of the buffer and returns it back to the pool.

--- internal/helper/byte.go
+++ internal/helper/byte.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"bytes"
+	"sync"
 )
 
 // ByteSliceHasAnyPrefix tests whether the byte slice s begins with any of the prefixes.
@@ -33,4 +34,23 @@ func UnquoteBytes(s []byte) []byte {
 	}
 
 	return s
+}
+
+// bytesBufferPool is a shared pool of the *bytes.Buffer instances for re-use.
+var bytesBufferPool = sync.Pool{New: func() interface{} {
+	return &bytes.Buffer{}
+}}
+
+// Buffer returns an instance of the *bytes.Buffer and a release function.
+// The release function should be called once the instance is not needed anymore.
+// It will allow for instance to be re-used without re-creation of it.
+// NOTE: be careful with usage of the bytes.Buffer.Bytes method as the underling
+// bytes buffer will be reset once release function called. If you need to reference
+// it later you need to copy it explicitly.
+func Buffer() (*bytes.Buffer, func()) {
+	buf := bytesBufferPool.Get().(*bytes.Buffer)
+	return buf, func() {
+		buf.Reset()
+		bytesBufferPool.Put(buf)
+	}
 }
