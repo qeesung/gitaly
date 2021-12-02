5103345014055dfcce1fe9227ef2696cf6991fe6
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-12-02 08:21:36 +0100

    pgbouncer kill

--- internal/praefect/datastore/glsql/testing.go
+++ internal/praefect/datastore/glsql/testing.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"net"
 	"os"
 	"os/exec"
@@ -182,23 +183,28 @@ func requireSQLOpen(t testing.TB, dbCfg config.DB, direct bool) *sql.DB {
 
 func requireTerminateAllConnections(t testing.TB, db *sql.DB, database string) {
 	t.Helper()
-	_, err := db.Exec("SELECT PG_TERMINATE_BACKEND(pid) FROM PG_STAT_ACTIVITY WHERE datname = '" + database + "'")
-	require.NoError(t, err)
 
-	// Once the pg_terminate_backend has completed, we may need to wait before the connections
-	// are fully released. pg_terminate_backend will return true as long as the signal was
-	// sent successfully, but the backend needs to respond to the signal to close the connection.
-	// TODO: In Postgre 14, pg_terminate_backend takes an optional timeout argument that makes it a blocking
-	// call. https://gitlab.com/gitlab-org/gitaly/-/issues/3937 tracks the refactor work to  remove this
-	// require.Eventuallyf call in favor of passing in a timeout to pg_terminate_backend
-	require.Eventuallyf(t, func() bool {
-		var openConnections int
-		require.NoError(t, db.QueryRow(
-			`SELECT COUNT(*) FROM pg_stat_activity
+	if _, ok := getDatabaseEnvironment(t)["PGHOST_PGBOUNCER"]; ok {
+		_, err := db.Exec(fmt.Sprintf("KILL %[1]s ; RESUME %[1]s", database))
+		require.NoError(t, err)
+	} else {
+		_, err := db.Exec("SELECT PG_TERMINATE_BACKEND(pid) FROM PG_STAT_ACTIVITY WHERE datname = '" + database + "'")
+		require.NoError(t, err)
+		// Once the pg_terminate_backend has completed, we may need to wait before the connections
+		// are fully released. pg_terminate_backend will return true as long as the signal was
+		// sent successfully, but the backend needs to respond to the signal to close the connection.
+		// TODO: In Postgre 14, pg_terminate_backend takes an optional timeout argument that makes it a blocking
+		// call. https://gitlab.com/gitlab-org/gitaly/-/issues/3937 tracks the refactor work to  remove this
+		// require.Eventuallyf call in favor of passing in a timeout to pg_terminate_backend
+		require.Eventuallyf(t, func() bool {
+			var openConnections int
+			require.NoError(t, db.QueryRow(
+				`SELECT COUNT(*) FROM pg_stat_activity
 				WHERE datname = $1 AND pid != pg_backend_pid()`, database).
-			Scan(&openConnections))
-		return openConnections == 0
-	}, 20*time.Second, 10*time.Millisecond, "wait for all connections to be terminated")
+				Scan(&openConnections))
+			return openConnections == 0
+		}, 20*time.Second, 10*time.Millisecond, "wait for all connections to be terminated")
+	}
 }
 
 func initPraefectTestDB(t testing.TB, database string) *sql.DB {
