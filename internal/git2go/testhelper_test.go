cc54b8d5debd6d8af1eaa933f7b4fcd99c9a2bac
Savely Krasovsky <savely@krasovs.ky>
2022-08-20 21:13:16 +0300

    fix(git2go): TestMain moved

--- internal/git2go/testhelper_test.go
+++ internal/git2go/testhelper_test.go
@@ -3,9 +3,16 @@
 package git2go
 
 import (
+	"testing"
+
 	git "github.com/libgit2/git2go/v33"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git/gittest"
+	"gitlab.com/gitlab-org/gitaly/v15/internal/testhelper"
 )
+
+func TestMain(m *testing.M) {
+	testhelper.Run(m)
+}
 
 var defaultAuthor = git.Signature{
 	Name:  gittest.DefaultCommitterName,
