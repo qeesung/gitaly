02b26b6b953832f89c1298378c61e6eabdf65025
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2023-09-08 17:54:24 +0700

    limiter: Add use_resizable_semaphore_in_concurrency_limiter feature flag

---
+++ internal/featureflag/ff_use_resizable_semaphore_in_concurrency_limiter.go
@@ -0,0 +1,11 @@
+package featureflag
+
+// UseResizableSemaphoreInConcurrencyLimiter enables the usage of limiter.resizableSemaphore in
+// limiter.ConcurrencyLimiter. After the flag is enabled, the implementation switched, but the limiter still uses the
+// static limit. There shouldn't be any changes in terms of functionality and resource usage.
+var UseResizableSemaphoreInConcurrencyLimiter = NewFeatureFlag(
+	"use_resizable_semaphore_in_concurrency_limiter",
+	"v16.5.0",
+	"https://gitlab.com/gitlab-org/gitaly/-/issues/5581",
+	false,
+)
