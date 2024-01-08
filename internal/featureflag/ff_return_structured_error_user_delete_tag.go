7298094e43a62db0438ea40ae80f91f9b3d21ec4
freedisch <freeproduc@gmail.com>
2024-01-08 03:59:49 +0200

    userdeleteTag under feature flag
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- internal/featureflag/ff_return_structured_error_user_delete_tag.go
+++ internal/featureflag/ff_return_structured_error_user_delete_tag.go
@@ -1,11 +1,10 @@
 package featureflag
 
-// ReturnStructuredErrorsInUserDeleteTag enables return structured errors in UserDeleteBranch.
+// ReturnStructuredErrorsInUserDeleteTag enables return structured errors in UserDeleteTag
 // Modify the RPC UserDeleteBranch to return structured errors instead of
 // inline errors. Modify the handling of the following four
 // errors: 'Access Check', 'Reference Update', and 'CustomHookError'.
 // Returns the corresponding structured error.
-
 var ReturnStructuredErrorsInUserDeleteTag = NewFeatureFlag(
 	"return_structured_errors_in_user_delete_tag",
 	"v16.8.1",
