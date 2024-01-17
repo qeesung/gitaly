fd98354cbadeeb749b01bdb7c21e8d7b3ece93c2
Eric Ju <eju@gitlab.com>
2024-01-17 15:14:24 -0400

    repository: Call GetFileAttributes instead of GetInfoAttributes
    In git 2.43, we will start get gitattributes from HEAD:.gitattributes
    instead of info/attributes.
    As a result, GetInfoAttributes is deprecated, we use
    GetFileAttributes gRPC call to replace it.

--- internal/gitaly/service/repository/apply_gitattributes.go
+++ internal/gitaly/service/repository/apply_gitattributes.go
@@ -3,13 +3,9 @@ package repository
 import (
 	"context"
 	"fmt"
-	"os"
 
-	"gitlab.com/gitlab-org/gitaly/v16/internal/helper/perm"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
 )
-
-const attributesFileMode os.FileMode = perm.SharedFile
 
 func (s *server) ApplyGitattributes(ctx context.Context, in *gitalypb.ApplyGitattributesRequest) (*gitalypb.ApplyGitattributesResponse, error) {
 	// In git 2.43.0+, gitattributes supports reading from HEAD:.gitattributes,
--- internal/gitaly/service/repository/file_attributes.go
+++ internal/gitaly/service/repository/file_attributes.go
@@ -3,14 +3,15 @@ package repository
 import (
 	"context"
 	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/gitattributes"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
-	"os"
-	"path/filepath"
 )
 
 func (s *server) GetFileAttributes(ctx context.Context, in *gitalypb.GetFileAttributesRequest) (*gitalypb.GetFileAttributesResponse, error) {
@@ -19,6 +20,20 @@ func (s *server) GetFileAttributes(ctx context.Context, in *gitalypb.GetFileAttr
 	}
 
 	repo := s.localrepo(in.GetRepository())
+
+	// In git 2.43.0+, gitattributes supports reading from HEAD:.gitattributes,
+	// so info/attributes is no longer needed. To make sure info/attributes file is cleaned up,
+	// we delete it if it exists when reading from HEAD:.gitattributes is called.
+	// This logic can be removed when ApplyGitattributes and GetInfoAttributes RPC are totally removed from
+	// the code base.
+	repoPath, err := s.locator.GetRepoPath(repo)
+	if err != nil {
+		return nil, fmt.Errorf("fail to get repo path: %w", err)
+	}
+
+	if deletionErr := deleteInfoAttributesFile(repoPath); deletionErr != nil {
+		return nil, fmt.Errorf("fail to delete info/gitattributes file at %s: %w", repoPath, err)
+	}
 
 	checkAttrCmd, finishAttr, err := gitattributes.CheckAttr(ctx, repo, git.Revision(in.GetRevision()), in.GetAttributes())
 	if err != nil {
