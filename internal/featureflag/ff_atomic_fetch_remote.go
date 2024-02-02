f153cf6fefb7cc4d967bc9d4fb747770dac5bd30
Justin Tobler <jtobler@gitlab.com>
2024-02-01 21:50:21 -0600

    featureflag: Remove `AtomicFetchRemote`
    With 5109854e (featureflag: Remove `AtomicFetchRemote`, 2024-01-08), the
    `AtomicFetchRemote` feature flag uses were removed but the flag
    definition left. Remove the feature flag completely.

--- internal/featureflag/ff_atomic_fetch_remote.go
+++
@@ -1,9 +0,0 @@
-package featureflag
-
-// AtomicFetchRemote enables atomic transactions for the `FetchRemote` RPC.
-var AtomicFetchRemote = NewFeatureFlag(
-	"atomic_fetch_remote",
-	"v16.5.0",
-	"https://gitlab.com/gitlab-org/gitaly/-/issues/5640",
-	true,
-)
