32669a922653b17976ba87834f583461f2232fc7
Sami Hiltunen <shiltunen@gitlab.com>
2024-02-09 10:38:52 +0200

    Deduplicate test setup in Entry tests
    Entry tests have some duplication between the test boilerplate of
    the two methods. Deduplicate the boilerplate as we're about to add
    a third method that can also reuse it.

--- internal/gitaly/storage/wal/entry_test.go
+++ internal/gitaly/storage/wal/entry_test.go
@@ -32,200 +32,121 @@ func TestEntry(t *testing.T) {
 	setupTestDirectory(t, filepath.Join(storageRoot, firstLevelDir))
 	setupTestDirectory(t, filepath.Join(storageRoot, secondLevelDir))
 
-	t.Run("RecordDirectoryCreation", func(t *testing.T) {
-		t.Parallel()
-
-		for _, tc := range []struct {
-			desc               string
-			directory          string
-			expectedOperations operations
-		}{
-			{
-				desc:      "first level",
-				directory: firstLevelDir,
-				expectedOperations: func() operations {
-					var ops operations
-					ops.flush("first-op")
-					ops.createDirectory("test-dir", perm.PrivateDir)
-					ops.createHardLink("1", "test-dir/file-1", false)
-					ops.createDirectory("test-dir/subdir-private", perm.PrivateDir)
-					ops.createHardLink("2", "test-dir/subdir-private/file-2", false)
-					ops.flush("test-dir/subdir-private")
-					ops.createDirectory("test-dir/subdir-shared", perm.SharedDir)
-					ops.createHardLink("3", "test-dir/subdir-shared/file-3", false)
-					ops.flush("test-dir/subdir-shared")
-					ops.flush("test-dir")
-					ops.flush(".")
-					return ops
-				}(),
+	for _, tc := range []struct {
+		desc               string
+		run                func(*testing.T, *Entry)
+		expectedOperations operations
+		expectedFiles      testhelper.DirectoryState
+	}{
+		{
+			desc: "RecordDirectoryCreation on first level directory",
+			run: func(t *testing.T, entry *Entry) {
+				require.NoError(t, entry.RecordDirectoryCreation(storageRoot, firstLevelDir))
 			},
-			{
-				desc:      "second level",
-				directory: secondLevelDir,
-				expectedOperations: func() operations {
-					var ops operations
-					ops.flush("first-op")
-					ops.createDirectory("second-level/test-dir", perm.PrivateDir)
-					ops.createHardLink("1", "second-level/test-dir/file-1", false)
-					ops.createDirectory("second-level/test-dir/subdir-private", perm.PrivateDir)
-					ops.createHardLink("2", "second-level/test-dir/subdir-private/file-2", false)
-					ops.flush("second-level/test-dir/subdir-private")
-					ops.createDirectory("second-level/test-dir/subdir-shared", perm.SharedDir)
-					ops.createHardLink("3", "second-level/test-dir/subdir-shared/file-3", false)
-					ops.flush("second-level/test-dir/subdir-shared")
-					ops.flush("second-level/test-dir")
-					ops.flush("second-level")
-					return ops
-				}(),
+			expectedOperations: func() operations {
+				var ops operations
+				ops.flush("sentinel-op")
+				ops.createDirectory("test-dir", perm.PrivateDir)
+				ops.createHardLink("1", "test-dir/file-1", false)
+				ops.createDirectory("test-dir/subdir-private", perm.PrivateDir)
+				ops.createHardLink("2", "test-dir/subdir-private/file-2", false)
+				ops.flush("test-dir/subdir-private")
+				ops.createDirectory("test-dir/subdir-shared", perm.SharedDir)
+				ops.createHardLink("3", "test-dir/subdir-shared/file-3", false)
+				ops.flush("test-dir/subdir-shared")
+				ops.flush("test-dir")
+				ops.flush(".")
+				return ops
+			}(),
+			expectedFiles: testhelper.DirectoryState{
+				"/":  {Mode: fs.ModeDir | perm.SharedDir},
+				"/1": {Mode: perm.PrivateExecutable, Content: []byte("file-1")},
+				"/2": {Mode: perm.SharedFile, Content: []byte("file-2")},
+				"/3": {Mode: perm.PrivateFile, Content: []byte("file-3")},
 			},
-		} {
-			tc := tc
-			t.Run(tc.desc, func(t *testing.T) {
-				t.Parallel()
-
-				stateDir := t.TempDir()
-				entry := NewEntry(stateDir)
-				entry.operations.flush("first-op")
-
-				require.NoError(t, entry.RecordDirectoryCreation(storageRoot, tc.directory))
-
-				testhelper.ProtoEqual(t, tc.expectedOperations, entry.operations)
-				testhelper.RequireDirectoryState(t, stateDir, "", testhelper.DirectoryState{
-					"/":  {Mode: fs.ModeDir | perm.SharedDir},
-					"/1": {Mode: perm.PrivateExecutable, Content: []byte("file-1")},
-					"/2": {Mode: perm.SharedFile, Content: []byte("file-2")},
-					"/3": {Mode: perm.PrivateFile, Content: []byte("file-3")},
-				})
-			})
-		}
-	})
-
-	t.Run("RecordDirectoryDeletion", func(t *testing.T) {
-		t.Parallel()
-
-		for _, tc := range []struct {
-			desc               string
-			directory          string
-			expectedOperations operations
-		}{
-			{
-				desc:      "first level",
-				directory: firstLevelDir,
-				expectedOperations: func() operations {
-					var ops operations
-					ops.removeDirectoryEntry("test-dir/file-1")
-					ops.removeDirectoryEntry("test-dir/subdir-private/file-2")
-					ops.removeDirectoryEntry("test-dir/subdir-private")
-					ops.removeDirectoryEntry("test-dir/subdir-shared/file-3")
-					ops.removeDirectoryEntry("test-dir/subdir-shared")
-					ops.removeDirectoryEntry("test-dir")
-					ops.flush(".")
-					ops.flush("first-op")
-					return ops
-				}(),
+		},
+		{
+			desc: "RecordDirectoryCreation on second level directory",
+			run: func(t *testing.T, entry *Entry) {
+				require.NoError(t, entry.RecordDirectoryCreation(storageRoot, secondLevelDir))
 			},
-			{
-				desc:      "second level",
-				directory: secondLevelDir,
-				expectedOperations: func() operations {
-					var ops operations
-					ops.removeDirectoryEntry("second-level/test-dir/file-1")
-					ops.removeDirectoryEntry("second-level/test-dir/subdir-private/file-2")
-					ops.removeDirectoryEntry("second-level/test-dir/subdir-private")
-					ops.removeDirectoryEntry("second-level/test-dir/subdir-shared/file-3")
-					ops.removeDirectoryEntry("second-level/test-dir/subdir-shared")
-					ops.removeDirectoryEntry("second-level/test-dir")
-					ops.flush("second-level")
-					ops.flush("first-op")
-					return ops
-				}(),
+			expectedOperations: func() operations {
+				var ops operations
+				ops.flush("sentinel-op")
+				ops.createDirectory("second-level/test-dir", perm.PrivateDir)
+				ops.createHardLink("1", "second-level/test-dir/file-1", false)
+				ops.createDirectory("second-level/test-dir/subdir-private", perm.PrivateDir)
+				ops.createHardLink("2", "second-level/test-dir/subdir-private/file-2", false)
+				ops.flush("second-level/test-dir/subdir-private")
+				ops.createDirectory("second-level/test-dir/subdir-shared", perm.SharedDir)
+				ops.createHardLink("3", "second-level/test-dir/subdir-shared/file-3", false)
+				ops.flush("second-level/test-dir/subdir-shared")
+				ops.flush("second-level/test-dir")
+				ops.flush("second-level")
+				return ops
+			}(),
+			expectedFiles: testhelper.DirectoryState{
+				"/":  {Mode: fs.ModeDir | perm.SharedDir},
+				"/1": {Mode: perm.PrivateExecutable, Content: []byte("file-1")},
+				"/2": {Mode: perm.SharedFile, Content: []byte("file-2")},
+				"/3": {Mode: perm.PrivateFile, Content: []byte("file-3")},
 			},
-		} {
-			tc := tc
-			t.Run(tc.desc, func(t *testing.T) {
-				t.Parallel()
-
-				stateDir := t.TempDir()
-				entry := NewEntry(stateDir)
-				entry.operations.flush("first-op")
-
-				require.NoError(t, entry.RecordDirectoryRemoval(storageRoot, tc.directory))
-
-				testhelper.ProtoEqual(t, tc.expectedOperations, entry.operations)
-				testhelper.RequireDirectoryState(t, stateDir, "", testhelper.DirectoryState{
-					"/": {Mode: fs.ModeDir | perm.SharedDir},
-				})
-			})
-		}
-	})
-
-	t.Run("RecordRepositoryCreation", func(t *testing.T) {
-		t.Parallel()
-
-		for _, tc := range []struct {
-			desc               string
-			directory          string
-			expectedOperations operations
-		}{
-			{
-				desc:      "first level",
-				directory: firstLevelDir,
-				expectedOperations: func() operations {
-					var ops operations
-					ops.flush("sentinel-op")
-					ops.createDirectory("test-dir", perm.PrivateDir)
-					ops.createHardLink("1", "test-dir/file-1", false)
-					ops.createDirectory("test-dir/subdir-private", perm.PrivateDir)
-					ops.createHardLink("2", "test-dir/subdir-private/file-2", false)
-					ops.flush("test-dir/subdir-private")
-					ops.createDirectory("test-dir/subdir-shared", perm.SharedDir)
-					ops.createHardLink("3", "test-dir/subdir-shared/file-3", false)
-					ops.flush("test-dir/subdir-shared")
-					ops.flush("test-dir")
-					ops.flush(".")
-					return ops
-				}(),
+		},
+		{
+			desc: "RecordDirectoryRemoval on first level directory",
+			run: func(t *testing.T, entry *Entry) {
+				require.NoError(t, entry.RecordDirectoryRemoval(storageRoot, firstLevelDir))
 			},
-			{
-				desc:      "second level",
-				directory: secondLevelDir,
-				expectedOperations: func() operations {
-					var ops operations
-					ops.flush("sentinel-op")
-					ops.createDirectory("second-level", perm.PrivateDir)
-					ops.createDirectory("second-level/test-dir", perm.PrivateDir)
-					ops.createHardLink("1", "second-level/test-dir/file-1", false)
-					ops.createDirectory("second-level/test-dir/subdir-private", perm.PrivateDir)
-					ops.createHardLink("2", "second-level/test-dir/subdir-private/file-2", false)
-					ops.flush("second-level/test-dir/subdir-private")
-					ops.createDirectory("second-level/test-dir/subdir-shared", perm.SharedDir)
-					ops.createHardLink("3", "second-level/test-dir/subdir-shared/file-3", false)
-					ops.flush("second-level/test-dir/subdir-shared")
-					ops.flush("second-level/test-dir")
-					ops.flush("second-level")
-					ops.flush(".")
-					return ops
-				}(),
+			expectedOperations: func() operations {
+				var ops operations
+				ops.removeDirectoryEntry("test-dir/file-1")
+				ops.removeDirectoryEntry("test-dir/subdir-private/file-2")
+				ops.removeDirectoryEntry("test-dir/subdir-private")
+				ops.removeDirectoryEntry("test-dir/subdir-shared/file-3")
+				ops.removeDirectoryEntry("test-dir/subdir-shared")
+				ops.removeDirectoryEntry("test-dir")
+				ops.flush(".")
+				ops.flush("sentinel-op")
+				return ops
+			}(),
+			expectedFiles: testhelper.DirectoryState{
+				"/": {Mode: fs.ModeDir | perm.SharedDir},
 			},
-		} {
-			tc := tc
-			t.Run(tc.desc, func(t *testing.T) {
-				t.Parallel()
+		},
+		{
+			desc: "RecordDirectoryRemoval on second level directory",
+			run: func(t *testing.T, entry *Entry) {
+				require.NoError(t, entry.RecordDirectoryRemoval(storageRoot, secondLevelDir))
+			},
+			expectedOperations: func() operations {
+				var ops operations
+				ops.removeDirectoryEntry("second-level/test-dir/file-1")
+				ops.removeDirectoryEntry("second-level/test-dir/subdir-private/file-2")
+				ops.removeDirectoryEntry("second-level/test-dir/subdir-private")
+				ops.removeDirectoryEntry("second-level/test-dir/subdir-shared/file-3")
+				ops.removeDirectoryEntry("second-level/test-dir/subdir-shared")
+				ops.removeDirectoryEntry("second-level/test-dir")
+				ops.flush("second-level")
+				ops.flush("sentinel-op")
+				return ops
+			}(),
+			expectedFiles: testhelper.DirectoryState{
+				"/": {Mode: fs.ModeDir | perm.SharedDir},
+			},
+		},
+	} {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
 
-				stateDir := t.TempDir()
-				entry := NewEntry(stateDir)
-				entry.operations.flush("sentinel-op")
+			stateDir := t.TempDir()
+			entry := NewEntry(stateDir)
+			entry.operations.flush("sentinel-op")
 
-				require.NoError(t, entry.RecordRepositoryCreation(storageRoot, tc.directory))
+			tc.run(t, entry)
 
-				testhelper.ProtoEqual(t, tc.expectedOperations, entry.operations)
-				testhelper.RequireDirectoryState(t, stateDir, "", testhelper.DirectoryState{
-					"/":  {Mode: fs.ModeDir | perm.SharedDir},
-					"/1": {Mode: perm.PrivateExecutable, Content: []byte("file-1")},
-					"/2": {Mode: perm.SharedFile, Content: []byte("file-2")},
-					"/3": {Mode: perm.PrivateFile, Content: []byte("file-3")},
-				})
-			})
-		}
-	})
+			testhelper.ProtoEqual(t, tc.expectedOperations, entry.operations)
+			testhelper.RequireDirectoryState(t, stateDir, "", tc.expectedFiles)
+		})
+	}
 }
