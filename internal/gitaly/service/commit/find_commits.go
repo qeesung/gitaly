qeesung/gitaly

commit 588d901fae1109e3e4883fd21c8ac00aa1afd59b

commit: Wait for the git-log(1) process to exit in FindCommits
In this commit, we add a new wait logic to wait for git-log(1)
command to finish;

Previously we did not Wait() for the git-log(1) process to exit
in FindCommits, causing us to fail to include its execution time
in the command details and not log any errors it returned.

Changelog: fixed

internal/gitaly/service/commit/find_commits.go
@@ -65,14 +65,22 @@ func (s *server) FindCommits(req *gitalypb.FindCommitsRequest, stream gitalypb.C
 	return nil
 }
 
-func (s *server) findCommits(ctx context.Context, req *gitalypb.FindCommitsRequest, stream gitalypb.CommitService_FindCommitsServer) error {
+func (s *server) findCommits(ctx context.Context, req *gitalypb.FindCommitsRequest, stream gitalypb.CommitService_FindCommitsServer) (err error) {
 	opts := git.ConvertGlobalOptions(req.GetGlobalOptions())
 	repo := s.localrepo(req.GetRepository())
 
-	logCmd, err := repo.Exec(ctx, getLogCommandSubCmd(req), append(opts, git.WithSetupStdout())...)
+	var stderr bytes.Buffer
+	gitLogCmd := getLogCommandSubCmd(req)
+	logCmd, err := repo.Exec(ctx, gitLogCmd, append(opts, git.WithSetupStdout(), git.WithStderr(&stderr))...)
 	if err != nil {
 		return fmt.Errorf("error when creating git log command: %w", err)
 	}
+
+	defer func() {
+		if err = logCmd.Wait(); err != nil {
+			err = wrapGitLogCmdError(req.GetRevision(), err, stderr.String())
+		}
+	}()
 
 	objectReader, cancel, err := s.catfileCache.ObjectReader(ctx, repo)
 	if err != nil {
@@ -98,6 +106,7 @@ func (s *server) findCommits(ctx context.Context, req *gitalypb.FindCommitsReque
 	if err := streamCommits(getCommits, stream, req.GetTrailers(), req.GetIncludeShortstat(), len(req.GetIncludeReferencedBy()) > 0); err != nil {
 		return fmt.Errorf("error streaming commits: %w", err)
 	}
+
 	return nil
 }
 
@@ -390,4 +399,27 @@ func splitStat(data []byte, atEOF bool) (int, []byte, error) {
 	}
 
 	return index + 1, data[:index], nil
+}
+
+var (
+	ambiguousArgRegex         = regexp.MustCompile(`fatal: ambiguous argument '.+': unknown revision or path not in the working tree.`)
+	badObjectRegex            = regexp.MustCompile(`fatal: bad object [0-9a-g]+`)
+	invalidRevisionRangeRegex = regexp.MustCompile(`fatal: Invalid revision range [0-9a-g]+\.\.[0-9a-g]+`)
+)
+
+// wrapGitError wraps git log error with a structError.
+func wrapGitLogCmdError(revision []byte, err error, stderr string) structerr.Error {
+	switch {
+	case ambiguousArgRegex.MatchString(stderr):
+		fallthrough
+	case badObjectRegex.MatchString(stderr):
+		fallthrough
+	case invalidRevisionRangeRegex.MatchString(stderr):
+		// for example git log 37811987837aacbd3b1d8ceb8de669b33f7c7c0a..37811987837aacbd3b1d8ceb8de669b33f7c7c0b will cause
+		// fatal: Invalid revision range 37811987837aacbd3b1d8ceb8de669b33f7c7c0a..37811987837aacbd3b1d8ceb8de669b33f7c7c0b
+		return structerr.NewNotFound("commits not found").
+			WithDetail(&gitalypb.FindCommitsError{})
+	default:
+		return structerr.NewInternal("listing commits failed").WithMetadata("stderr", stderr)
+	}
 }

internal/gitaly/service/commit/find_commits_test.go
@@ -620,6 +620,54 @@ func TestFindCommits(t *testing.T) {
 				}
 			},
 		},
+		{
+			desc: "no commits found due to ambiguous argument",
+			setup: func(t *testing.T) setupData {
+				repo, _ := gittest.CreateRepository(t, ctx, cfg)
+				return setupData{
+					request: &gitalypb.FindCommitsRequest{
+						Repository:       repo,
+						Revision:         []byte("non-existing-ref"),
+						Limit:            9000,
+						IncludeShortstat: true,
+					},
+					expectedErr: structerr.NewNotFound("commits not found").
+						WithDetail(&gitalypb.FindCommitsError{}),
+				}
+			},
+		},
+		{
+			desc: "no commits found due to bad object id",
+			setup: func(t *testing.T) setupData {
+				repo, _ := gittest.CreateRepository(t, ctx, cfg)
+				return setupData{
+					request: &gitalypb.FindCommitsRequest{
+						Repository:       repo,
+						Revision:         []byte("37811987837aacbd3b1d8ceb8de669b33f7c7c0a"),
+						Limit:            9000,
+						IncludeShortstat: true,
+					},
+					expectedErr: structerr.NewNotFound("commits not found").
+						WithDetail(&gitalypb.FindCommitsError{}),
+				}
+			},
+		},
+		{
+			desc: "no commits found due to invalid range",
+			setup: func(t *testing.T) setupData {
+				repo, _ := gittest.CreateRepository(t, ctx, cfg)
+				return setupData{
+					request: &gitalypb.FindCommitsRequest{
+						Repository:       repo,
+						Revision:         []byte("37811987837aacbd3b1d8ceb8de669b33f7c7c0a..37811987837aacbd3b1d8ceb8de669b33f7c7c0b"),
+						Limit:            9000,
+						IncludeShortstat: true,
+					},
+					expectedErr: structerr.NewNotFound("commits not found").
+						WithDetail(&gitalypb.FindCommitsError{}),
+				}
+			},
+		},
 	} {
 		tc := tc
 
@@ -651,6 +699,7 @@ func TestFindCommits_quarantine(t *testing.T) {
 		desc          string
 		altDirs       []string
 		expectedCount int
+		expectedErr   error
 	}{
 		{
 			desc:          "present GIT_ALTERNATE_OBJECT_DIRECTORIES",
@@ -661,6 +710,8 @@ func TestFindCommits_quarantine(t *testing.T) {
 			desc:          "empty GIT_ALTERNATE_OBJECT_DIRECTORIES",
 			altDirs:       []string{},
 			expectedCount: 0,
+			expectedErr: structerr.NewNotFound("commits not found").
+				WithDetail(&gitalypb.FindCommitsError{}),
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
@@ -681,7 +732,7 @@ func TestFindCommits_quarantine(t *testing.T) {
 				Revision:   []byte(commitID.String()),
 				Limit:      1,
 			})
-			require.NoError(t, err)
+			testhelper.RequireGrpcError(t, tc.expectedErr, err)
 			require.Len(t, commits, tc.expectedCount)
 		})
 	}

commit ef5840b9843e7014e85d8b0755f39083e47be5b7

Fix FindCommits in repositories missing default branches
A regression was introduced in FindCommits recently in
588d901fae1109e3e4883fd21c8ac00aa1afd59b. The commit fixed the RPC
to actually wait for the git log command and return a possible error
that may be raised. This surfaced an error where listing the commits
fail if the repository is empty. While this case seems to have always
errored out, the error started to be only returned after the error
handling was fixed. This has lead to behavior changes in Rails' commit
listing API that is now failing with internal server error on empty
repositories.

Fix the issue by relaxing the regex that is checking for the ambiguous
argument case. It currently expects the revision to have at least one
character. In the fallback cases in GetDefaultBranch(), an empty string
is returned if the repository has no branches. This leads to triggering
'git log ""' which fails due to the non-existent revision.

Changelog: fixed

internal/gitaly/service/commit/find_commits.go
@@ -402,7 +402,7 @@ func splitStat(data []byte, atEOF bool) (int, []byte, error) {
 }
 
 var (
-	ambiguousArgRegex         = regexp.MustCompile(`fatal: ambiguous argument '.+': unknown revision or path not in the working tree.`)
+	ambiguousArgRegex         = regexp.MustCompile(`fatal: ambiguous argument '.*': unknown revision or path not in the working tree.`)
 	badObjectRegex            = regexp.MustCompile(`fatal: bad object [0-9a-g]+`)
 	invalidRevisionRangeRegex = regexp.MustCompile(`fatal: Invalid revision range [0-9a-g]+\.\.[0-9a-g]+`)
 )

internal/gitaly/service/commit/find_commits_test.go
@@ -18,7 +18,6 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
-	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb/testproto"
 	"golang.org/x/text/encoding/charmap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
@@ -65,10 +64,7 @@ func TestFindCommits(t *testing.T) {
 					request: &gitalypb.FindCommitsRequest{
 						Repository: repoProto,
 					},
-					expectedErr: structerr.NewInternal("listing commits failed").WithDetail(&testproto.ErrorMetadata{
-						Key:   []byte("stderr"),
-						Value: []byte("fatal: ambiguous argument '': unknown revision or path not in the working tree.\nUse '--' to separate paths from revisions, like this:\n'git <command> [<revision>...] -- [<file>...]'\n"),
-					}),
+					expectedErr: structerr.NewNotFound("commits not found").WithDetail(&gitalypb.FindCommitsError{}),
 				}
 			},
 		},