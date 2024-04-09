574f2747d81e7fdbe2f520e023d1c60f207f80c1
Sami Hiltunen <shiltunen@gitlab.com>
2024-04-09 22:26:35 +0300

    Add helpers for passing partitioning hints via gRPC metadata
    Gitaly generally relies on the additional repository in the request
    to determine if the request's target repository needs to be partitioned
    with another repository. In some cases we don't have an additional
    repository in the request but we know at the time of sending the request
    that the target should be partitioned with another repository. An example
    of this is when an object pool is first replicated to a Gitaly node, and
    afterwards its members. As we're replicating the member, we know it should
    join the pool and that we'll link it later. The ReplicateRepository request
    itself doesn't contain enough information to determine this though. The
    solution is to send Gitaly a partitioning hint when creating the repository
    that needs to be partitioned with another one. That way we can eventually
    then link the repositories together.
    
    This commit adds helpers for passing partitioning hints via gRPC metadata.

--- internal/gitaly/storage/storagectx/transaction_test.go
+++ internal/gitaly/storage/storagectx/transaction_test.go
@@ -2,12 +2,14 @@ package storagectx
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
 	housekeepingcfg "gitlab.com/gitlab-org/gitaly/v16/internal/git/housekeeping/config"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
+	grpc_metadata "google.golang.org/grpc/metadata"
 )
 
 type nilTransaction struct {
@@ -57,5 +59,49 @@ func TestContextWithTransaction(t *testing.T) {
 		)
 
 		require.True(t, callbackRan)
+	})
+}
+
+func TestPartitioningHint(t *testing.T) {
+	t.Run("no hint provided", func(t *testing.T) {
+		ctx := context.Background()
+
+		relativePath, err := ExtractPartitioningHintFromIncomingContext(ctx)
+		require.NoError(t, err)
+		require.Empty(t, relativePath)
+	})
+
+	t.Run("hint provided", func(t *testing.T) {
+		ctx := SetPartitioningHintToIncomingContext(context.Background(), "relative-path")
+
+		relativePath, err := ExtractPartitioningHintFromIncomingContext(ctx)
+		require.NoError(t, err)
+		require.Equal(t, relativePath, "relative-path")
+	})
+
+	t.Run("doesn't modify original metadata", func(t *testing.T) {
+		originalMetadata := grpc_metadata.New(nil)
+		originalCtx := grpc_metadata.NewIncomingContext(context.Background(), originalMetadata)
+
+		ctx := SetPartitioningHintToIncomingContext(originalCtx, "relative-path")
+
+		relativePath, err := ExtractPartitioningHintFromIncomingContext(ctx)
+		require.NoError(t, err)
+		require.Equal(t, relativePath, "relative-path")
+
+		relativePath, err = ExtractPartitioningHintFromIncomingContext(originalCtx)
+		require.NoError(t, err)
+		require.Empty(t, relativePath)
+	})
+
+	t.Run("fails if multiple hints set", func(t *testing.T) {
+		md := grpc_metadata.New(nil)
+		md.Set(keyPartitioningHint, "relative-path-1", "relative-path-2")
+
+		relativePath, err := ExtractPartitioningHintFromIncomingContext(
+			grpc_metadata.NewIncomingContext(context.Background(), md),
+		)
+		require.Equal(t, errors.New("multiple partitioning hints"), err)
+		require.Empty(t, relativePath)
 	})
 }
