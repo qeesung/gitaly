0781947d7a9da628ce72d56236974b6bbf58c13d
Justin Tobler <jtobler@gitlab.com>
2024-05-16 16:18:20 -0500

    server: Wire dependencies to `ReceivePack` RPCs
    To use the proc-receive hook, the `SSHReceivePack` and `PostReceivePack`
    RPCs need `hook.Manager`, `storagemgr.TransactionRegistry`, and
    `updateref.UpdaterWithHooks`. Wire these dependencies to both of the
    servers.

--- internal/gitaly/service/smarthttp/server.go
+++ internal/gitaly/service/smarthttp/server.go
@@ -5,8 +5,13 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v16/internal/backup"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/bundleuri"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/catfile"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/config"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/hook"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/hook/updateref"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/service"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/storagemgr"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/transaction"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/log"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
@@ -15,11 +20,16 @@ import (
 type server struct {
 	gitalypb.UnimplementedSmartHTTPServiceServer
 	logger                     log.Logger
+	cfg                        config.Cfg
 	locator                    storage.Locator
 	gitCmdFactory              git.CommandFactory
+	catfileCache               catfile.Cache
 	packfileNegotiationMetrics *prometheus.CounterVec
 	infoRefCache               infoRefCache
 	txManager                  transaction.Manager
+	txRegistry                 *storagemgr.TransactionRegistry
+	hookManager                hook.Manager
+	updater                    *updateref.UpdaterWithHooks
 	backupLocator              backup.Locator
 	backupSink                 backup.Sink
 	bundleURISink              *bundleuri.Sink
@@ -29,9 +39,14 @@ type server struct {
 func NewServer(deps *service.Dependencies, serverOpts ...ServerOpt) gitalypb.SmartHTTPServiceServer {
 	s := &server{
 		logger:        deps.GetLogger(),
+		cfg:           deps.GetCfg(),
 		locator:       deps.GetLocator(),
 		gitCmdFactory: deps.GetGitCmdFactory(),
+		catfileCache:  deps.GetCatfileCache(),
 		txManager:     deps.GetTxManager(),
+		txRegistry:    deps.GetTransactionRegistry(),
+		hookManager:   deps.GetHookManager(),
+		updater:       deps.GetUpdaterWithHooks(),
 		packfileNegotiationMetrics: prometheus.NewCounterVec(
 			prometheus.CounterOpts{},
 			[]string{"git_negotiation_feature"},
--- internal/gitaly/service/ssh/server.go
+++ internal/gitaly/service/ssh/server.go
@@ -5,8 +5,13 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v16/internal/backup"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/bundleuri"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/catfile"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/config"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/hook"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/hook/updateref"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/service"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/storagemgr"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/transaction"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/helper"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/log"
@@ -16,9 +21,14 @@ import (
 type server struct {
 	gitalypb.UnimplementedSSHServiceServer
 	logger                                   log.Logger
+	cfg                                      config.Cfg
 	locator                                  storage.Locator
 	gitCmdFactory                            git.CommandFactory
+	catfileCache                             catfile.Cache
 	txManager                                transaction.Manager
+	txRegistry                               *storagemgr.TransactionRegistry
+	hookManager                              hook.Manager
+	updater                                  *updateref.UpdaterWithHooks
 	uploadPackRequestTimeoutTickerFactory    func() helper.Ticker
 	uploadArchiveRequestTimeoutTickerFactory func() helper.Ticker
 	packfileNegotiationMetrics               *prometheus.CounterVec
@@ -31,9 +41,14 @@ type server struct {
 func NewServer(deps *service.Dependencies, serverOpts ...ServerOpt) gitalypb.SSHServiceServer {
 	s := &server{
 		logger:        deps.GetLogger(),
+		cfg:           deps.GetCfg(),
 		locator:       deps.GetLocator(),
 		gitCmdFactory: deps.GetGitCmdFactory(),
+		catfileCache:  deps.GetCatfileCache(),
 		txManager:     deps.GetTxManager(),
+		txRegistry:    deps.GetTransactionRegistry(),
+		hookManager:   deps.GetHookManager(),
+		updater:       deps.GetUpdaterWithHooks(),
 		uploadPackRequestTimeoutTickerFactory: func() helper.Ticker {
 			return helper.NewTimerTicker(deps.Cfg.Timeout.UploadPackNegotiation.Duration())
 		},
