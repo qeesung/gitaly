e349d6ffd4a8f3563bbe86c1ea44cfa2df94c64a
Sami Hiltunen <shiltunen@gitlab.com>
2024-06-03 18:27:25 +0300

    Test concurrently getting partition of non-existent repository
    Add a test covering concurrently getting partition ID of a
    non-existing repository. The test reveals an issue where we don't
    return the expected `relativePathNotFoundError` when a goroutine
    is blocked behind another goroutine while getting a partition ID.

--- internal/gitaly/storage/storagemgr/partition_assigner_test.go
+++ internal/gitaly/storage/storagemgr/partition_assigner_test.go
@@ -482,3 +482,58 @@ func TestPartitionAssigner_concurrentAccess(t *testing.T) {
 		})
 	}
 }
+
+type wrappedContext struct {
+	context.Context
+	doneCalled chan struct{}
+}
+
+func (w wrappedContext) Done() <-chan struct{} {
+	close(w.doneCalled)
+	return w.Context.Done()
+}
+
+func TestPartitionAssigner_concurrent_access_non_existent(t *testing.T) {
+	t.Parallel()
+
+	ctx := testhelper.Context(t)
+
+	db, err := keyvalue.NewBadgerStore(testhelper.SharedLogger(t), t.TempDir())
+	require.NoError(t, err)
+	defer testhelper.MustClose(t, db)
+
+	cfg := testcfg.Build(t)
+
+	pa, err := newPartitionAssigner(db, cfg.Storages[0].Path)
+	require.NoError(t, err)
+	defer testhelper.MustClose(t, pa)
+
+	relativePath := "relative-path"
+	pa.repositoryLocks[relativePath] = make(chan struct{})
+
+	var (
+		isBlocked         = make(chan struct{})
+		actualPartitionID storage.PartitionID
+		actualError       error
+		wg                sync.WaitGroup
+	)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		actualPartitionID, actualError = pa.getPartitionID(wrappedContext{
+			Context:    ctx,
+			doneCalled: isBlocked,
+		}, relativePath, "", false)
+	}()
+
+	<-isBlocked
+	close(pa.repositoryLocks[relativePath])
+
+	wg.Wait()
+
+	// This is a bug. We'd actually want a relativePathNotFoundError returned
+	// here as returned when there's no concurrency.
+	require.ErrorIs(t, actualError, errPartitionAssignmentNotFound)
+	require.Zero(t, actualPartitionID)
+}
