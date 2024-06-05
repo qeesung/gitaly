d9dd88cec0832635602512228e1e373076df251d
Karthik Nayak <knayak@gitlab.com>
2024-06-05 17:15:56 +0200

    updateref: Add support for 'symref-update'
    Add support for the upcoming 'symref-update' subcommand, which allows us
    to update symrefs. Since the feature is yet to be released in a new Git
    version, we block it behind a high Git version.

--- internal/git/version.go
+++ internal/git/version.go
@@ -64,6 +64,14 @@ func parseVersionOutput(versionOutput []byte) (Version, error) {
 	return version, nil
 }
 
+// SupportSymrefUpdates validates if the version supports 'symref-update' subcommands
+// in 'git-update-ref'. This feature has not been tagged yet, so we use a high
+// version number to build for it. This should be changed as the feature gets merged
+// and released.
+func (v Version) SupportSymrefUpdates() bool {
+	return v.GreaterOrEqual(NewVersion(99, 99, 99, 0))
+}
+
 // String returns the string representation of the version.
 func (v Version) String() string {
 	return v.versionString
